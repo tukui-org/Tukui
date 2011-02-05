59e3c6b24429e818cb70f7c102b2ff3de0b9f505
Elv22 <flipside_22@live.com>
2011-02-05 16:43:39 -0500

    Add T.RGBToHex function.

--- Tukui/core/functions.lua
+++ Tukui/core/functions.lua
@@ -235,6 +235,13 @@ T.Round = function(number, decimals)
     return (("%%.%df"):format(decimals)):format(number)
 end
 
+T.RGBToHex = function(r, g, b)
+	r = r <= 1 and r >= 0 and r or 0
+	g = g <= 1 and g >= 0 and g or 0
+	b = b <= 1 and b >= 0 and b or 0
+	return string.format("|cff%02x%02x%02x", r*255, g*255, b*255)
+end
+
 T.RGBPercToHex = function(r, g, b)
 	r = r <= 1 and r >= 0 and r or 0
 	g = g <= 1 and g >= 0 and g or 0
