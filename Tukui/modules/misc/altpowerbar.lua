4c9f4d19387bf57d9abb2197cea7730576a1846e
Tukz <tukz@tukui.org>
2011-02-07 16:15:19 -0500

    outline sux

--- Tukui/modules/misc/altpowerbar.lua
+++ Tukui/modules/misc/altpowerbar.lua
@@ -25,7 +25,7 @@ AltPowerBarStatus:Point("BOTTOMRIGHT", TukuiInfoLeft, "BOTTOMRIGHT", -2, 2)
 AltPowerBarStatus:SetStatusBarColor(75/255,  175/255, 76/255)
 
 local AltPowerText = AltPowerBarStatus:CreateFontString(nil, "OVERLAY")
-AltPowerText:SetFont(C.media.font, 12, "OUTLINE")
+AltPowerText:SetFont(C.media.font, 12)
 AltPowerText:Point("CENTER", AltPowerBar, "CENTER", 0, 0)
 
 --Event handling
