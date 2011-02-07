0e64222334e50669a4cde2ca7516f765005f5e11
Tukz <tukz@tukui.org>
2011-02-07 15:59:59 -0500

    use this eyefinity damage script according to our blizzard damage/healing settings.

--- Tukui/modules/misc/damage.lua
+++ Tukui/modules/misc/damage.lua
@@ -7,6 +7,10 @@ local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, vari
 
 if not T.eyefinity then return end
 
+local displaydamage = GetCVar("CombatDamage")
+local displayheal = GetCVar("CombatHealing")
+local displaydot = GetCVar("CombatLogPeriodicSpells")
+
 local gflags = bit.bor(COMBATLOG_OBJECT_AFFILIATION_MINE, COMBATLOG_OBJECT_REACTION_FRIENDLY, COMBATLOG_OBJECT_CONTROL_PLAYER, COMBATLOG_OBJECT_TYPE_GUARDIAN)
 	
 local function OnEvent(self, event, ...)
@@ -14,31 +18,35 @@ local function OnEvent(self, event, ...)
 
 	if sourceGUID == UnitGUID("player") or sourceGUID == UnitGUID("pet") or sourceFlags == gflags then
 		-- dmg
-		if eventType == "SWING_DAMAGE" then
-			local amount, _, _, _, _, _, critical = select(9, ...)
-			self:AddMessage(amount, 1, 1, 1)
-		elseif eventType == "SPELL_DAMAGE" or eventType == "SPELL_PERIODIC_DAMAGE" then
-			local spellId, _, spellSchool, amount, _, _, _, _, _, critical = select(9, ...)
-			if eventType == "SPELL_PERIODIC_DAMAGE" then
-				self:AddMessage(amount, 151/255, 70/255, 194/255)
-			else
-				self:AddMessage(amount, 1, 1, 0)
+		if displaydamage then
+			if eventType == "SWING_DAMAGE" then
+				local amount, _, _, _, _, _, critical = select(9, ...)
+				self:AddMessage(amount, 1, 1, 1)
+			elseif eventType == "SPELL_DAMAGE" or eventType == "SPELL_PERIODIC_DAMAGE" then
+				local spellId, _, spellSchool, amount, _, _, _, _, _, critical = select(9, ...)
+				if eventType == "SPELL_PERIODIC_DAMAGE" then
+					if displaydot then self:AddMessage(amount, 151/255, 70/255, 194/255) end
+				else
+					self:AddMessage(amount, 1, 1, 0)
+				end
+			elseif eventType == "RANGE_DAMAGE" then
+				local spellId, _, _, amount, _, _, _, _, _, critical = select(9, ...)
+				self:AddMessage(amount, 1, 1, 1)
+			elseif eventType == "SWING_MISSED" then
+				local missType, _ = select(9, ...)
+				self:AddMessage(missType, 1, 1, 1)
+			elseif eventType == "SPELL_MISSED" or eventType == "RANGE_MISSED" then
+				local spellId, _, _, missType, _ = select(9,...)
+				self:AddMessage(missType, 1, 1, 1)
 			end
-		elseif eventType == "RANGE_DAMAGE" then
-			local spellId, _, _, amount, _, _, _, _, _, critical = select(9, ...)
-			self:AddMessage(amount, 1, 1, 1)
-		elseif eventType == "SWING_MISSED" then
-			local missType, _ = select(9, ...)
-			self:AddMessage(missType, 1, 1, 1)
-		elseif eventType == "SPELL_MISSED" or eventType == "RANGE_MISSED" then
-			local spellId, _, _, missType, _ = select(9,...)
-			self:AddMessage(missType, 1, 1, 1)
 		end
 		
 		-- heal
-		if eventType == "SPELL_HEAL" or eventType== "SPELL_PERIODIC_HEAL" then
-			local spellId, spellName, spellSchool, amount, overhealing, absorbed, critical = select(9,...)
-			self:AddMessage(amount, 0, 1, 0)			
+		if displayheal then
+			if eventType == "SPELL_HEAL" or eventType== "SPELL_PERIODIC_HEAL" then
+				local spellId, spellName, spellSchool, amount, overhealing, absorbed, critical = select(9,...)
+				self:AddMessage(amount, 0, 1, 0)			
+			end
 		end
 	end
 end
@@ -55,3 +63,34 @@ f:SetFadeDuration(0.5)
 f:SetTimeVisible(1)
 f:SetMaxLines(64)
 f:SetSpacing(2)
+
+local o = CreateFrame("Frame")
+o:RegisterEvent("CVAR_UPDATE")
+o:SetScript("OnEvent", function(self, event, cvar, value)
+	if cvar == "SHOW_DAMAGE_TEXT" then
+		if value == 1 then
+			displaydamage = true
+		else
+			displaydamage = false
+		end
+	end
+	
+	if cvar == "LOG_PERIODIC_EFFECTS" then
+		if value == 1 then
+			displaydot = true
+		else
+			displaydot = false
+		end
+	end
+	
+	if cvar == "SHOW_COMBAT_HEALING" then
+		if value == 1 then
+			displayheal = true
+		else
+			displayheal = false
+		end
+	end
+end)
+
+-- kill
+InterfaceOptionsCombatTextPanelPetDamage:Kill()
