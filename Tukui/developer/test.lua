bd6fa62d63f7be76305e81dfbb6bb9b5a451007b
Tukz <tukz@tukui.org>
2011-03-06 00:44:17 -0500

    remove deprecated and force error to be show for beta

--- Tukui/developer/test.lua
+++ Tukui/developer/test.lua
@@ -4,4 +4,5 @@ local beta = CreateFrame("Frame")
 beta:RegisterEvent("PLAYER_ENTERING_WORLD")
 beta:SetScript("OnEvent", function(self, event)
 	print("|CFFFF0000THIS IS A BETA VERSION OF TUKUI FOR 4.1.0 PTR ONLY|r")
+	SetCVar("scriptErrors", 1) -- force it for development
 end)
