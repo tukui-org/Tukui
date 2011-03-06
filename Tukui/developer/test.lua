e00bd440f75dcbad4b865a6b6d6934105929ea7f
Tukz <tukz@tukui.org>
2011-03-06 00:23:58 -0500

    add a beta msg

--- Tukui/developer/test.lua
+++ Tukui/developer/test.lua
@@ -1,1 +1,7 @@
 local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
+
+local beta = CreateFrame("Frame")
+beta:RegisterEvent("PLAYER_ENTERING_WORLD")
+beta:SetScript("OnEvent", function(self, event)
+	print("|CFFFF0000THIS IS A BETA VERSION OF TUKUI FOR 4.1.0 PTR ONLY|r")
+end)
