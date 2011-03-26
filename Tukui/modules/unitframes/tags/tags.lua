b71e42349a2ffc1648c2bf6b7114a4674f895626
Tukz <tukz@tukui.org>
2011-03-26 03:11:20 -0400

    remove hapiness, build 04

--- Tukui/modules/unitframes/tags/tags.lua
+++ Tukui/modules/unitframes/tags/tags.lua
@@ -117,10 +117,7 @@ end
 oUF.TagEvents['Tukui:getnamecolor'] = 'UNIT_POWER'
 oUF.Tags['Tukui:getnamecolor'] = function(unit)
 	local reaction = UnitReaction(unit, 'player')
-	if (unit == 'pet' and SPELL_POWER_HAPPINESS and GetPetHappiness()) then
-		local c = T.oUF_colors.happiness[GetPetHappiness()]
-		return string.format('|cff%02x%02x%02x', c[1] * 255, c[2] * 255, c[3] * 255)
-	elseif (UnitIsPlayer(unit)) then
+	if (UnitIsPlayer(unit)) then
 		return _TAGS['raidcolor'](unit)
 	elseif (reaction) then
 		local c = T.oUF_colors.reaction[reaction]
