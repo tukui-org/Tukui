1b05104cd0402d2e718bf44abb93db1b4f985a0f
Tukz <tukz@tukui.org>
2011-03-26 03:44:45 -0400

    gradient color for alt and threat

--- Tukui/modules/misc/altpowerbar.lua
+++ Tukui/modules/misc/altpowerbar.lua
@@ -1,34 +1,30 @@
-local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
-
+local T, C, L = unpack(select(2, ...))
 if IsAddOnLoaded("SmellyPowerBar") then return end
 
--- Get rid of old Alt Power Bar
 PlayerPowerBarAlt:UnregisterEvent("UNIT_POWER_BAR_SHOW")
 PlayerPowerBarAlt:UnregisterEvent("UNIT_POWER_BAR_HIDE")
 PlayerPowerBarAlt:UnregisterEvent("PLAYER_ENTERING_WORLD")
 	
---Create the new bar
 local AltPowerBar = CreateFrame("Frame", "TukuiAltPowerBar", TukuiInfoLeft)
 AltPowerBar:SetAllPoints()
-AltPowerBar:SetFrameStrata("LOW")
+AltPowerBar:SetFrameStrata("MEDIUM")
 AltPowerBar:SetFrameLevel(0)
 AltPowerBar:EnableMouse(true)
 AltPowerBar:SetTemplate("Default")
 
--- Create Status Bar and Text
 local AltPowerBarStatus = CreateFrame("StatusBar", "TukuiAltPowerBarStatus", AltPowerBar)
 AltPowerBarStatus:SetFrameLevel(AltPowerBar:GetFrameLevel() + 1)
 AltPowerBarStatus:SetStatusBarTexture(C.media.normTex)
 AltPowerBarStatus:SetMinMaxValues(0, 100)
 AltPowerBarStatus:Point("TOPLEFT", TukuiInfoLeft, "TOPLEFT", 2, -2)
 AltPowerBarStatus:Point("BOTTOMRIGHT", TukuiInfoLeft, "BOTTOMRIGHT", -2, 2)
-AltPowerBarStatus:SetStatusBarColor(75/255,  175/255, 76/255)
 
 local AltPowerText = AltPowerBarStatus:CreateFontString(nil, "OVERLAY")
 AltPowerText:SetFont(C.media.font, 12)
 AltPowerText:Point("CENTER", AltPowerBar, "CENTER", 0, 0)
+AltPowerText:SetShadowColor(0, 0, 0)
+AltPowerText:SetShadowOffset(1.25, -1.25)
 
---Event handling
 AltPowerBar:RegisterEvent("UNIT_POWER")
 AltPowerBar:RegisterEvent("UNIT_POWER_BAR_SHOW")
 AltPowerBar:RegisterEvent("UNIT_POWER_BAR_HIDE")
@@ -42,7 +38,6 @@ AltPowerBar:SetScript("OnEvent", function(self)
 	end
 end)
 
--- Update Functions
 local TimeSinceLastUpdate = 1
 AltPowerBarStatus:SetScript("OnUpdate", function(self, elapsed)
 	if not AltPowerBar:IsShown() then return end
@@ -53,7 +48,9 @@ AltPowerBarStatus:SetScript("OnUpdate", function(self, elapsed)
 		local power = UnitPower("player", ALTERNATE_POWER_INDEX)
 		local mpower = UnitPowerMax("player", ALTERNATE_POWER_INDEX)
 		self:SetValue(power)
-		AltPowerText:SetText(power.."/"..mpower)
+		AltPowerText:SetText(power.." / "..mpower)
+		local r, g, b = oUFTukui.ColorGradient(power/mpower, 0,.8,0,.8,.8,0,.8,0,0)
+		AltPowerBarStatus:SetStatusBarColor(r, g, b)
 		self.TimeSinceLastUpdate = 0
 	end
 end)
