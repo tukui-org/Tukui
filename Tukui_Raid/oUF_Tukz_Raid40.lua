d151920984084980cac42f70e1298b3f49de82fb
Tukz <tukz@tukui.org>
2011-06-27 14:43:56 -0400

    add backdrop to healing layout

--- Tukui_Raid/oUF_Tukz_Raid40.lua
+++ Tukui_Raid/oUF_Tukz_Raid40.lua
@@ -10,6 +10,10 @@ if not C["unitframes"].enable == true then return end
 
 local font2 = C["media"].uffont
 local font1 = C["media"].font
+local backdrop = {
+	bgFile = C["media"].blank,
+	insets = {top = -T.mult, left = -T.mult, bottom = -T.mult, right = -T.mult},
+}
 
 local function Shared(self, unit)
 	self.colors = T.oUF_colors
