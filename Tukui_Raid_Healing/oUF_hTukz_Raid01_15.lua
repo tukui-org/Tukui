1c1b8f1b7ace12406758d786a1746d0e5806f6d1
Tukz <tukz@tukui.org>
2011-07-04 23:43:08 -0400

    backdrop was missing on weakened soul bar

--- Tukui_Raid_Healing/oUF_hTukz_Raid01_15.lua
+++ Tukui_Raid_Healing/oUF_hTukz_Raid01_15.lua
@@ -11,6 +11,10 @@ if not C["unitframes"].enable == true or C["unitframes"].gridonly == true then r
 local font2 = C["media"].uffont
 local font1 = C["media"].font
 local normTex = C["media"].normTex
+local backdrop = {
+	bgFile = C["media"].blank,
+	insets = {top = -T.mult, left = -T.mult, bottom = -T.mult, right = -T.mult},
+}
 
 local function Shared(self, unit)
 	self.colors = T.oUF_colors
