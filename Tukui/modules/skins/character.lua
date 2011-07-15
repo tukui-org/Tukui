8d96cccb79f465123bb3cc7c29bdd74fdb5cf571
Tukz <tukz@tukui.org>
2011-07-15 14:08:26 -0400

    Bug Fix : taint with Character Frame skin.

--- Tukui/modules/skins/character.lua
+++ Tukui/modules/skins/character.lua
@@ -131,20 +131,7 @@ local function LoadSkin()
 
 			object.Check:SetTexture(nil)
 			object.icon:SetTexCoord(.08, .92, .08, .92)
-			
-			if not object.backdrop then
-				object:CreateBackdrop("Default")
-			end
-			
-			object.backdrop:Point("TOPLEFT", object.icon, "TOPLEFT", -2, 2)
-			object.backdrop:Point("BOTTOMRIGHT", object.icon, "BOTTOMRIGHT", 2, -2)
-			object.icon:SetParent(object.backdrop)
-
-			--Making all icons the same size and position because otherwise BlizzardUI tries to attach itself to itself when it refreshes
-			object.icon:SetPoint("LEFT", object, "LEFT", 4, 0)
-			object.icon.SetPoint = T.dummy
-			object.icon:Size(36, 36)
-			object.icon.SetSize = T.dummy
+			object.icon:SetTemplate("Default")
 		end
 		GearManagerDialogPopup:StripTextures()
 		GearManagerDialogPopup:SetTemplate("Default")
