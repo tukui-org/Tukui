a9c23a413225a263d2f10ff4790e1efe564a0885
Tukz <tukz@tukui.org>
2011-07-29 17:46:45 -0400

    Bug Fix : http://www.tukui.org/forums/topic.php?id=14996

--- Tukui/modules/skins/questlog.lua
+++ Tukui/modules/skins/questlog.lua
@@ -13,6 +13,9 @@ local function LoadSkin()
 	QuestLogFrameShowMapButton.text:ClearAllPoints()
 	QuestLogFrameShowMapButton.text:SetPoint("CENTER")
 	QuestLogFrameShowMapButton:Size(QuestLogFrameShowMapButton:GetWidth() - 30, QuestLogFrameShowMapButton:GetHeight(), - 40)
+	
+	QuestLogFrameCompleteButton:StripTextures()
+	T.SkinButton(QuestLogFrameCompleteButton)
 
 	local buttons = {
 		"QuestLogFrameAbandonButton",
