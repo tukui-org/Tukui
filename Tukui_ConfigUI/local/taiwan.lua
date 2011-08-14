755aea0e4e8d065ea4753d79fc4efa5cbb5fb7b4
Tukz <tukz@tukui.org>
2011-08-14 17:44:14 -0400

    taiwan local update

--- Tukui_ConfigUI/local/taiwan.lua
+++ Tukui_ConfigUI/local/taiwan.lua
@@ -11,7 +11,7 @@
 	TukuiL.option_general_customuiscale = "使用者介面比例(需要關閉自動調整)"
 	TukuiL.option_general_backdropcolor = "設定面板背景顏色"
 	TukuiL.option_general_bordercolor = "設定面板邊框顏色"
-	TukuiL.option_general_blizzardreskin = "Reskin Blizzard frames to match Tukui"
+	TukuiL.option_general_blizzardreskin = "使用TUKUI風格代替暴雪默認界面"
  
 	-- nameplate
 	TukuiL.option_nameplates = "名條"
@@ -180,8 +180,8 @@
 	TukuiL.option_chat_background = "啟用對話框背景"
 		
 	-- buff
-	TukuiL.option_auras = "Auras"
-	TukuiL.option_auras_player = "Enable Tukui Buff/Debuff Frames"
+	TukuiL.option_auras = "顯示光環"
+	TukuiL.option_auras_player = "啟用 Tukui Buff/Debuff 框體"
  
 	-- buttons
 	TukuiL.option_button_reset = "重置"
