7e6a71ce5f4be04139626495d5539e6414b1f3d4
Tukz <tukz@tukui.org>
2011-08-16 16:02:13 -0400

    Bug Fix : Change resolution detection to allow secondary monitor to be used.

--- Tukui/core/uiscale.lua
+++ Tukui/core/uiscale.lua
@@ -39,7 +39,7 @@ T.UIScale = function()
 	end
 	
 	if C["general"].autoscale == true then
-		C["general"].uiscale = min(2, max(.64, 768/string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)")))
+		C["general"].uiscale = min(2, max(.64, 768/string.match(T.resolution, "%d+x(%d+)")))
 	end
 
 	if T.lowversion then
