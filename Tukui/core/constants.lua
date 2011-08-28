3e0217aeb099ba1c99b914bb6872562673783670
Tukz <tukz@tukui.org>
2011-08-28 11:44:02 -0400

    Bug Fix : Hit DataText and Draenei Racial.

--- Tukui/core/constants.lua
+++ Tukui/core/constants.lua
@@ -3,6 +3,7 @@ local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, vari
 T.dummy = function() return end
 T.myname = select(1, UnitName("player"))
 T.myclass = select(2, UnitClass("player"))
+T.myrace = select(2, UnitRace("player"))
 T.client = GetLocale() 
 T.resolution = GetCVar("gxResolution")
 T.getscreenheight = tonumber(string.match(T.resolution, "%d+x(%d+)"))
--- Tukui/modules/datatext/hit.lua
+++ Tukui/modules/datatext/hit.lua
@@ -12,6 +12,9 @@ if not C["datatext"].hit == nil or C["datatext"].hit > 0 then
 	T.PP(C["datatext"].hit, Text)
 
 	local int = 1
+	local racial = 0
+	
+	if T.myrace == "Draenei" then racial = 1 end
 
 	local function Update(self, t)
 		int = int - t
@@ -26,11 +29,11 @@ if not C["datatext"].hit == nil or C["datatext"].hit > 0 then
 
 		if int < 0 then
 			if attackpwr > spellpwr and select(2, UnitClass("Player")) ~= "HUNTER" then
-				Text:SetText(format("%.2f", GetCombatRatingBonus(6)).."% Hit")
+				Text:SetText(format("%.2f", GetCombatRatingBonus(6)+racial).."% Hit")
 			elseif select(2, UnitClass("Player")) == "HUNTER" then
-				Text:SetText(format("%.2f", GetCombatRatingBonus(7)).."% Hit")
+				Text:SetText(format("%.2f", GetCombatRatingBonus(7)+racial).."% Hit")
 			else
-				Text:SetText(format("%.2f", GetCombatRatingBonus(8)).."% Hit")
+				Text:SetText(format("%.2f", GetCombatRatingBonus(8)+racial).."% Hit")
 			end
 			int = 1
 		end
