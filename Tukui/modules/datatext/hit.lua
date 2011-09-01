1bd99674217e4cb36f1d212190621a39182e27a3
Tukz <tukz@tukui.org>
2011-09-01 13:10:35 -0400

    Bug Fix : Hit Datatext lua error on login, and reduce calling value interval

--- Tukui/modules/datatext/hit.lua
+++ Tukui/modules/datatext/hit.lua
@@ -1,4 +1,5 @@
 local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
+
 --------------------------------------------------------------------
 -- Player Hit
 --------------------------------------------------------------------
@@ -13,21 +14,21 @@ if not C["datatext"].hit == nil or C["datatext"].hit > 0 then
 
 	local int = 1
 
-	local function Update(self, t)
+	local function Update(self, t)		
 		int = int - t
-		local base, posBuff, negBuff = UnitAttackPower("player")
-		local effective = base + posBuff + negBuff
-		local Rbase, RposBuff, RnegBuff = UnitRangedAttackPower("player")
-		local Reffective = Rbase + RposBuff + RnegBuff
-
-		Rattackpwr = Reffective
-		spellpwr = GetSpellBonusDamage(7)
-		attackpwr = effective
-		
-		local cac = GetHitModifier()
-		local cast = GetSpellHitModifier()
-
 		if int < 0 then
+			local base, posBuff, negBuff = UnitAttackPower("player")
+			local effective = base + posBuff + negBuff
+			local Rbase, RposBuff, RnegBuff = UnitRangedAttackPower("player")
+			local Reffective = Rbase + RposBuff + RnegBuff
+
+			local Rattackpwr = Reffective
+			local spellpwr = GetSpellBonusDamage(7)
+			local attackpwr = effective
+			
+			local cac = GetHitModifier() or 0
+			local cast = GetSpellHitModifier() or 0
+			
 			if attackpwr > spellpwr and select(2, UnitClass("Player")) ~= "HUNTER" then
 				Text:SetText(format("%.2f", GetCombatRatingBonus(6)+cac).."% Hit")
 			elseif select(2, UnitClass("Player")) == "HUNTER" then
@@ -35,6 +36,7 @@ if not C["datatext"].hit == nil or C["datatext"].hit > 0 then
 			else
 				Text:SetText(format("%.2f", GetCombatRatingBonus(8)+cast).."% Hit")
 			end
+			
 			int = 1
 		end
 	end
