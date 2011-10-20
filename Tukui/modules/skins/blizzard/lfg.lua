5587650ba26b8c4c742a621f4c431e8f1fd10c2f
Tukz <tukz@tukui.org>
2011-10-20 01:01:18 -0400

    update looking for guild skin

--- Tukui/modules/skins/blizzard/lfg.lua
+++ Tukui/modules/skins/blizzard/lfg.lua
@@ -48,9 +48,49 @@ local function LoadSkin()
 	for i = 1, 5 do
 		local b = _G["LookingForGuildBrowseFrameContainerButton"..i]
 		local t = _G["LookingForGuildAppsFrameContainerButton"..i]
+		local r = _G["LookingForGuildBrowseFrameContainerButton"..i.."Pending"]
 		b:SetBackdrop(nil)
+		b:SetTemplate("Default")
+		b:SetBackdropColor(0, 0, 0, 0)
+		b:SetBackdropBorderColor(1, 0, 0, 0)
+		b:StyleButton()
 		t:SetBackdrop(nil)
+		b.selectedTex:Kill()
+		if r then
+			r:StripTextures()
+			r:SetTemplate("Transparent")
+		end
+		
+		b.isSelected = false
 	end
+
+	-- skin apps request
+	hooksecurefunc("LookingForGuild_Update", function(self, button)
+		local s = GetRecruitingGuildSelection()
+		local f = LookingForGuildBrowseFrameContainer
+		local o = HybridScrollFrame_GetOffset(f)
+		local b = f.buttons
+		local nb = #b;
+		local button, index
+		
+		for i = 1, nb do
+			index = o + i
+			button = b[i]
+			if index == s then
+				if not button.isSelected then
+					button:SetBackdropBorderColor(1, 0, 0, 1)
+					button:SetBackdropColor(1, 1, 1, .1)
+					button.isSelected = true
+				end
+			else
+				if button.isSelected then
+					button:SetBackdropBorderColor(0, 0, 0, 0)
+					button:SetBackdropColor(0, 0, 0, 0)
+					button.isSelected = false
+				end
+			end
+		end
+	end)
 	
 	-- skin tabs
 	for i= 1, 3 do
@@ -62,7 +102,25 @@ local function LoadSkin()
 	T.SkinButton(GuildFinderRequestMembershipFrameAcceptButton)
 	T.SkinButton(GuildFinderRequestMembershipFrameCancelButton)
 	GuildFinderRequestMembershipFrameInputFrame:StripTextures()
-	GuildFinderRequestMembershipFrameInputFrame:SetTemplate("Default")		
+	GuildFinderRequestMembershipFrameInputFrame:SetTemplate("Default")
+
+	-- skin apps request
+	hooksecurefunc("LookingForGuildApps_Update", function()
+		local f = LookingForGuildAppsFrameContainer
+		local b = f.buttons
+		local nb = #b
+		local na = GetNumGuildMembershipRequests()
+		local button
+		
+		for i = 1, nb do
+			button = b[i]
+			if not button.isSkinned then
+				button:SetTemplate("Default")
+				button:StyleButton()
+				button.isSkinned = true
+			end
+		end
+	end)
 end
 
 T.SkinFuncs["Blizzard_LookingForGuildUI"] = LoadSkin
