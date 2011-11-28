eb95ad5f216dc12b77bdafc62ae7debe611c3ca6
unknown <tukz@desktop.(none)>
2011-11-27 19:20:27 -0500

    I don't remember who wrote this, but it was very buggy, fixing ...

--- Tukui/modules/datatext/talent.lua
+++ Tukui/modules/datatext/talent.lua
@@ -1,6 +1,6 @@
 ﻿local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
 
-if C.datatext.talent then
+if C.datatext.talent and C.datatext.talent > 0 then
 	local Stat = CreateFrame("Frame", "TukuiStatTalent")
 	Stat:EnableMouse(true)
 	Stat:SetFrameStrata("BACKGROUND")
@@ -13,71 +13,57 @@ if C.datatext.talent then
 	Text:SetFont(C.media.font, C["datatext"].fontsize)
 	T.PP(C.datatext.talent, Text)
  
-	local int = 1
-	local function Update(self, t)
-		if not GetPrimaryTalentTree() then
+	local function Update(self)
+		local majorTree = GetPrimaryTalentTree()
+		if not majorTree then
 			Text:SetText(L.datatext_notalents) 
-		return end
-		int = int - t
-		if int < 0 then
+		else
 			local tree1num = select(5,GetTalentTabInfo(1))
 			local tree2num = select(5,GetTalentTabInfo(2))
 			local tree3num = select(5,GetTalentTabInfo(3))
-			local majorTree = GetPrimaryTalentTree()
 			Text:SetText(Stat.Color1..select(2,GetTalentTabInfo(majorTree))..":|r "..Stat.Color2..tree1num.."/"..tree2num.."/"..tree3num)
 		end
-	end
- 
-	local function OnEvent(self, event, ...)
-		if event == "PLAYER_LOGIN" then
-			self:UnregisterEvent("PLAYER_LOGIN")
-		end
- 
-		local c = GetActiveTalentGroup(false,false)
-		local group1tree1 = select(5,GetTalentTabInfo(1,false,false,1))
-		local group1tree2 = select(5,GetTalentTabInfo(2,false,false,1))
-		local group1tree3 = select(5,GetTalentTabInfo(3,false,false,1))
-		local majorTree1 = GetPrimaryTalentTree(false,false,1)
-		local hs = (GetNumTalentGroups() == 2 and GetPrimaryTalentTree(false,false,2))
-		local group2tree1 = hs and select(5,GetTalentTabInfo(1,false,false,2))
-		local group2tree2 = hs and select(5,GetTalentTabInfo(2,false,false,2))
-		local group2tree3 = hs and select(5,GetTalentTabInfo(3,false,false,2))
-		local majorTree2 = 0
-		if hs then
-			majorTree2 = GetPrimaryTalentTree(false,false,2)
-		end
- 
-		-- Setup Talents Tooltip
 		self:SetAllPoints(Text)
- 
-		self:SetScript("OnEnter", function(self)
-			if not InCombatLockdown() then
-				local anchor, panel, xoff, yoff = T.DataTextTooltipAnchor(Text)	
-				GameTooltip:SetOwner(panel, anchor, xoff, yoff)
-				GameTooltip:ClearLines()
- 
-				if(not GetPrimaryTalentTree()) then
-					GameTooltip:AddLine(L.datatext_notalents)
-				elseif(hs) then
-					local green = "|cff00ff00("..ACTIVE_PETS..")"
-					local red = "|cffff0000 "
-					GameTooltip:AddDoubleLine("|cffffff00"..COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATESPEC1 ..":|r " .. select(2,GetTalentTabInfo(majorTree1)).." ("..group1tree1.."/"..group1tree2.."/"..group1tree3..")",(c == 1 and green or red) .. "|r",1,1,1)
-					GameTooltip:AddDoubleLine("|cffffff00"..COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATESPEC2 ..":|r ".. select(2,GetTalentTabInfo(majorTree2)).." ("..group2tree1.."/"..group2tree2.."/"..group2tree3..")",(c == 2 and green or red) .. "|r",1,1,1)
-				else
-					GameTooltip:AddLine(select(2,GetTalentTabInfo(majorTree1))..": "..group1tree1.."/"..group1tree2.."/"..group1tree3,1,1,1)
-				end
- 
-				GameTooltip:Show()
-			end
- 
-		end)
-		self:SetScript("OnLeave", function() GameTooltip:Hide() end)
 	end
  
 	Stat:RegisterEvent("PLAYER_LOGIN")
 	Stat:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-	Stat:SetScript("OnEvent", OnEvent)
-	Stat:SetScript("OnUpdate", Update)
+	Stat:RegisterEvent("CHARACTER_POINTS_CHANGED")
+	Stat:RegisterEvent("PLAYER_TALENT_UPDATE")
+	Stat:SetScript("OnEvent", Update)
+	Stat:SetScript("OnEnter", function(self)
+		if not InCombatLockdown() then
+			local anchor, panel, xoff, yoff = T.DataTextTooltipAnchor(Text)	
+			GameTooltip:SetOwner(panel, anchor, xoff, yoff)
+			GameTooltip:ClearLines()
+			
+			local c = GetActiveTalentGroup(false,false)
+			local group1tree1 = select(5,GetTalentTabInfo(1,false,false,1))
+			local group1tree2 = select(5,GetTalentTabInfo(2,false,false,1))
+			local group1tree3 = select(5,GetTalentTabInfo(3,false,false,1))
+			local majorTree1 = GetPrimaryTalentTree(false,false,1)
+			local hs = (GetNumTalentGroups() == 2 and GetPrimaryTalentTree(false,false,2))
+			local group2tree1 = hs and select(5,GetTalentTabInfo(1,false,false,2))
+			local group2tree2 = hs and select(5,GetTalentTabInfo(2,false,false,2))
+			local group2tree3 = hs and select(5,GetTalentTabInfo(3,false,false,2))
+			local majorTree2 = 0
+			if hs then
+				majorTree2 = GetPrimaryTalentTree(false,false,2)
+			end
+			
+			local green = "|cff00ff00("..ACTIVE_PETS..")"
+			local red = "|cffff0000 "
+			if majorTree1 and group1tree1 and group1tree2 and group1tree3 then
+				GameTooltip:AddDoubleLine("|cffffff00"..COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATESPEC1 ..":|r " .. select(2,GetTalentTabInfo(majorTree1)).." ("..group1tree1.."/"..group1tree2.."/"..group1tree3..")",(c == 1 and green or red) .. "|r",1,1,1)
+			end
+			if majorTree2 and group2tree1 and group2tree2 and group2tree3 then
+				GameTooltip:AddDoubleLine("|cffffff00"..COMPACT_UNIT_FRAME_PROFILE_AUTOACTIVATESPEC2 ..":|r ".. select(2,GetTalentTabInfo(majorTree2)).." ("..group2tree1.."/"..group2tree2.."/"..group2tree3..")",(c == 2 and green or red) .. "|r",1,1,1)
+			end
+
+			GameTooltip:Show()
+		end
+	end)
+	Stat:SetScript("OnLeave", function() GameTooltip:Hide() end)
 	Stat:SetScript("OnMouseDown", function()
 		c = GetActiveTalentGroup(false,false)
 		SetActiveTalentGroup(c == 1 and 2 or 1)
