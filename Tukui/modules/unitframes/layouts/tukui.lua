710ae09d4b4debc423efb063e12abfe865d7ce91
Tukz <tukz@tukui.org>
2012-08-29 15:22:56 -0400

    Typo from paladin holy power previous commit.

--- Tukui/modules/unitframes/layouts/tukui.lua
+++ Tukui/modules/unitframes/layouts/tukui.lua
@@ -570,11 +570,11 @@ local function Shared(self, unit)
 						
 						bars[i].bg:SetTexture(normTex)					
 						bars[i].bg:SetAlpha(.15)
+						bars[i].width = bars[i]:GetWidth()
 					end
 					
 					bars.Override = T.UpdateHoly
 					self.HolyPower = bars
-					bars[i].width = bars[i]:GetWidth()
 				end
 
 				-- deathknight runes
