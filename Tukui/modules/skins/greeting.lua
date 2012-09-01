b81d696fefb0036fb8b404f342c7cc6126c00b7d
Tukz <tukz@tukui.org>
2012-08-31 21:49:59 -0400

    Fix bug report #92. Quest Title being sometime black on a black frame.

--- Tukui/modules/skins/greeting.lua
+++ Tukui/modules/skins/greeting.lua
@@ -1,24 +1,28 @@
 local T, C, L, G = unpack(select(2, ...))
 
 local function LoadSkin()
-	QuestFrameGreetingPanel:HookScript("OnShow", function()
-		QuestFrameGreetingPanel:StripTextures()
-		QuestGreetingScrollFrame:StripTextures()
-		QuestFrameGreetingGoodbyeButton:SkinButton(true)
-		GreetingText:SetTextColor(1, 1, 1)
-		CurrentQuestsText:SetTextColor(1, 1, 0)
-		QuestGreetingFrameHorizontalBreak:Kill()
-		AvailableQuestsText:SetTextColor(1, 1, 0)
-		
-		for i=1, MAX_NUM_QUESTS do
-			local button = _G["QuestTitleButton"..i]
-			if button:GetFontString() then
-				if button:GetFontString():GetText() and button:GetFontString():GetText():find("|cff000000") then
-					button:GetFontString():SetText(string.gsub(button:GetFontString():GetText(), "|cff000000", "|cffFFFF00"))
+	QuestFrameGreetingPanel:StripTextures()
+	QuestGreetingScrollFrame:StripTextures()
+	QuestFrameGreetingGoodbyeButton:SkinButton(true)
+	QuestGreetingFrameHorizontalBreak:Kill()
+	GreetingText:SetTextColor(1, 1, 1)
+	GreetingText.SetTextColor = T.dummy
+	CurrentQuestsText:SetTextColor(1, 1, 0)
+	CurrentQuestsText.SetTextColor = T.dummy
+	AvailableQuestsText:SetTextColor(1, 1, 0)
+	AvailableQuestsText.SetTextColor = T.dummy
+	for i = 1, MAX_NUM_QUESTS do
+		local button = _G["QuestTitleButton"..i]
+		if button then
+			hooksecurefunc(button, "SetFormattedText", function()
+				if button:GetFontString() then
+					if button:GetFontString():GetText() and button:GetFontString():GetText():find("|cff000000") then
+						button:GetFontString():SetText(string.gsub(button:GetFontString():GetText(), "|cff000000", "|cffFFFF00"))
+					end
 				end
-			end
+			end)
 		end
-	end)
+	end
 end
 
 tinsert(T.SkinFuncs["Tukui"], LoadSkin)
