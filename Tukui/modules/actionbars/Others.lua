b7d1fd558a50c74b7203bc56957e22a3f6cc3431
Tukz <tukz@tukui.org>
2012-09-01 21:54:17 -0400

    I don't know why this was removed, but adding it again.

--- Tukui/modules/actionbars/Others.lua
+++ Tukui/modules/actionbars/Others.lua
@@ -36,5 +36,29 @@ TukuiOnLogon:SetScript("OnEvent", function(self, event)
 			T.ShowPopup("TUKUI_FIX_AB")
 		end
 	end
+	
+	for i = 1, 12 do
+		local button = _G[format("ActionButton%d", i)]
+		button:SetAttribute("showgrid", 1)
+		ActionButton_ShowGrid(button)
+		
+		button = _G[format("MultiBarRightButton%d", i)]
+		button:SetAttribute("showgrid", 1)
+		ActionButton_ShowGrid(button)
+
+		button = _G[format("MultiBarBottomRightButton%d", i)]
+		button:SetAttribute("showgrid", 1)
+		ActionButton_ShowGrid(button)
+		
+		button = _G[format("MultiBarLeftButton%d", i)]
+		button:SetAttribute("showgrid", 1)
+		ActionButton_ShowGrid(button)
+		
+		button = _G[format("MultiBarBottomLeftButton%d", i)]
+		button:SetAttribute("showgrid", 1)
+		ActionButton_ShowGrid(button)
+	end
+	ActionButton_HideGrid = T.dummy
+	ActionButton_ShowGrid = T.dummy
 end)
 G.ActionBars.EnterWorld = TukuiOnLogon
