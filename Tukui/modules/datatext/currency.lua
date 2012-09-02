610a6880ecf22db9958b90997c4a6b3cdacb9335
Tukz <tukz@tukui.org>
2012-09-02 15:23:14 -0400

    Currency name instead of "---" in currency datatext.

--- Tukui/modules/datatext/currency.lua
+++ Tukui/modules/datatext/currency.lua
@@ -19,7 +19,7 @@ if C["datatext"].currency and C["datatext"].currency > 0 then
 	G.DataText.Currency.Text = Text
 	
 	local function update()
-		local _text = Stat.Color2.."---".."|r"
+		local _text = Stat.Color2..CURRENCY.."|r"
 		for i = 1, MAX_WATCHED_TOKENS do
 			local name, count, _, _, _ = GetBackpackCurrencyInfo(i)
 			if name and count then
