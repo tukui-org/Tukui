c58bf7bc25c5a948426bc473a571ee045692e667
Tukz <tukz@tukui.org>
2012-09-08 16:08:59 -0400

    Fix bug report #130. (/rd command)

--- Tukui/modules/misc/commands.lua
+++ Tukui/modules/misc/commands.lua
@@ -10,38 +10,51 @@ SLASH_ENABLE_ADDON1 = "/enable"
 SlashCmdList.RCSLASH = DoReadyCheck
 SLASH_RCSLASH1 = "/rc"
 
+-- disband party/raid
+local function DisbandRaidGroup()
+	if InCombatLockdown() then return end
+	
+	if UnitInRaid("player") then
+		SendChatMessage(ERR_GROUP_DISBANDED, "RAID")
+		for i = 1, GetNumGroupMembers() do
+			local name, _, _, _, _, _, _, online = GetRaidRosterInfo(i)
+			if online and name ~= T.myname then
+				UninviteUnit(name)
+			end
+		end
+	else
+		SendChatMessage(ERR_GROUP_DISBANDED, "PARTY")
+		for i = MAX_PARTY_MEMBERS, 1, -1 do
+			if GetNumGroupMembers(i) then
+				UninviteUnit(UnitName("party"..i))
+			end
+		end
+	end
+	LeaveParty()
+end
+
 T.CreatePopup["TUKUIDISBAND_RAID"] = {
 	question = L.disband,
 	answer1 = ACCEPT,
 	answer2 = CANCEL,
-	function1 = function()
-		if InCombatLockdown() then return end -- Prevent user error in combat
-		
-		SendChatMessage(ERR_GROUP_DISBANDED, "RAID" or "PARTY")
-		if UnitInRaid("player") then
-			for i = 1, GetNumGroupMembers() do
-				local name, _, _, _, _, _, _, online = GetRaidRosterInfo(i)
-				if online and name ~= T.myname then
-					UninviteUnit(name)
-				end
-			end
-		else
-			for i = MAX_PARTY_MEMBERS, 1, -1 do
-				if GetPartyMember(i) then
-					UninviteUnit(UnitName("party"..i))
-				end
-			end
-		end
-		LeaveParty()	
-	end,
+	function1 = DisbandRaidGroup,
 }
 
 SlashCmdList["GROUPDISBAND"] = function()
-	if UnitIsRaidOfficer("player") then
+	local instanceType = select(2, IsInInstance())
+
+	-- don't allow disband in bg's, arena, tol barad, etc
+	if instanceType == "pvp" or instanceType == "arena" then
+		return
+	end
+	
+	-- only allow disband group if leader or assistant
+	if UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") then
 		T.ShowPopup("TUKUIDISBAND_RAID")
 	end
 end
-SLASH_GROUPDISBAND1 = '/rd'
+SLASH_GROUPDISBAND1 = '/gd'
+SLASH_GROUPDISBAND2 = '/rd'
 
 -- Leave party chat command
 SlashCmdList["LEAVEPARTY"] = function()
