3c38341872ce3df4b3d080ef376eaecc6ca1dbe0
Tukz <tukz@tukui.org>
2013-03-06 11:24:07 -0500

    fix warlock shard/embers.

--- Tukui/modules/unitframes/plugins/oUF_WarlockSpecBars/oUF_WarlockSpecBars.lua
+++ Tukui/modules/unitframes/plugins/oUF_WarlockSpecBars/oUF_WarlockSpecBars.lua
@@ -91,12 +91,7 @@ local function Visibility(self, event, unit)
 		end
 		
 		if spec == SPEC_WARLOCK_DESTRUCTION then
-			local maxembers = 3
-			
-			for i = 1, GetNumGlyphSockets() do
-				local glyphID = select(4, GetGlyphSocketInfo(i))
-				if glyphID == SPEC_WARLOCK_DESTRUCTION_GLYPH_EMBERS then maxembers = 4 end
-			end			
+			local maxembers = 4		
 
 			for i = 1, maxembers do
 				if i ~= maxembers then
@@ -108,15 +103,8 @@ local function Visibility(self, event, unit)
 				wsb[i]:SetStatusBarColor(unpack(Colors[SPEC_WARLOCK_DESTRUCTION]))
 				if wsb[i].bg then wsb[i].bg:SetAlpha(0.15) wsb[i].bg:SetTexture(unpack(Colors[SPEC_WARLOCK_DESTRUCTION])) end
 			end
-			
-			if maxembers == 3 then wsb[4]:Hide() else wsb[4]:Show() end
 		elseif spec == SPEC_WARLOCK_AFFLICTION then
-			local maxshards = 3
-			
-			for i = 1, GetNumGlyphSockets() do
-				local glyphID = select(4, GetGlyphSocketInfo(i))
-				if glyphID == SPEC_WARLOCK_AFFLICTION_GLYPH_SHARDS then maxshards = 4 end
-			end			
+			local maxshards = 4		
 
 			for i = 1, maxshards do
 				if i ~= maxshards then
@@ -128,8 +116,6 @@ local function Visibility(self, event, unit)
 				wsb[i]:SetStatusBarColor(unpack(Colors[SPEC_WARLOCK_AFFLICTION]))
 				if wsb[i].bg then wsb[i].bg:SetAlpha(0) end
 			end
-			
-			if maxshards == 3 then wsb[4]:Hide() else wsb[4]:Show() end
 		elseif spec == SPEC_WARLOCK_DEMONOLOGY then
 			wsb[2]:Hide()
 			wsb[3]:Hide()
