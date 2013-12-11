df263f36095305bb1aa26166716c9bafd0c4fc0e
Tukz <tukz@tukui.org>
2013-12-11 15:42:50 -0500

    Guild Datatext 5.4 bug fix.

--- Tukui/modules/datatext/guild.lua
+++ Tukui/modules/datatext/guild.lua
@@ -85,12 +85,9 @@ local function Update(self, event, ...)
 	end
 	
 	if IsInGuild() then
-		totalOnline = 0
-		local name, rank, level, zone, note, officernote, connected, status, class
-		for i = 1, GetNumGuildMembers() do
-			local connected = select(9, GetGuildRosterInfo(i))
-			if connected then totalOnline = totalOnline + 1 end
-		end	
+		GuildRoster() -- Bux Fix on 5.4.
+		totalOnline = select(3, GetNumGuildMembers())
+		
 		Text:SetFormattedText(displayString, L.datatext_guild, totalOnline)
 	else
 		Text:SetText(L.datatext_noguild)
