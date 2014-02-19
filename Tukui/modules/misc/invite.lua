3dd9cd44a9a3f2f9245da2696ea990aa9c5b3a0d
Tukz <tukz@tukui.org>
2014-02-19 17:26:40 -0500

    15.84, fix a lua error with auto-invites.

--- Tukui/modules/misc/invite.lua
+++ Tukui/modules/misc/invite.lua
@@ -34,8 +34,6 @@ if C["invite"].autoaccept then
 	ai:SetScript("OnEvent", function(self, event, name)
 		if QueueStatusMinimapButton:IsShown() or GetNumGroupMembers() > 0 then return end
 		if CheckFriend(name) then
-			RaidNotice_AddMessage(RaidWarningFrame, L_INFO_INVITE..name, {r = 0.41, g = 0.8, b = 0.94}, 3)
-			print(format("|cffffff00"..L_INFO_INVITE..name.."."))
 			AcceptGroup()
 			for i = 1, STATICPOPUP_NUMDIALOGS do
 				local frame = _G["StaticPopup"..i]
