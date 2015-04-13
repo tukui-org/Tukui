66a3617f619ae9e14678f8d9197befe39edd0886
Azilroka <darktemplar9900@yahoo.com>
2015-04-13 00:08:26 -0500

    Actually this is better (Same as last commit)

--- Tukui/Modules/ActionBars/Range.lua
+++ Tukui/Modules/ActionBars/Range.lua
@@ -21,29 +21,28 @@ function TukuiActionBars:RangeUpdate()
 	local Icon = self.icon
 	local NormalTexture = self.NormalTexture
     local ID = self.action
-    local IsUsable, NotEnoughMana, HasRange, InRange
 
-	if ID then
-		IsUsable, NotEnoughMana = IsUsableAction(ID)
-		HasRange = ActionHasRange(ID)
-		InRange = IsActionInRange(ID)
-	end
+	if not ID then return end
 
-    if IsUsable then -- Usable
-        if (HasRange and InRange == false) then -- Out of range
+	local IsUsable, NotEnoughMana = IsUsableAction(ID)
+	local HasRange = ActionHasRange(ID)
+	local InRange = IsActionInRange(ID)
+
+	if IsUsable then -- Usable
+		if (HasRange and InRange == false) then -- Out of range
 			Icon:SetVertexColor(0.8, 0.1, 0.1)
 			NormalTexture:SetVertexColor(0.8, 0.1, 0.1)
-        else -- In range
+		else -- In range
 			Icon:SetVertexColor(1.0, 1.0, 1.0)
 			NormalTexture:SetVertexColor(1.0, 1.0, 1.0)
-        end
-    elseif NotEnoughMana then -- Not enough power
+		end
+	elseif NotEnoughMana then -- Not enough power
 		Icon:SetVertexColor(0.1, 0.3, 1.0)
 		NormalTexture:SetVertexColor(0.1, 0.3, 1.0)
 	else -- Not usable
 		Icon:SetVertexColor(0.3, 0.3, 0.3)
 		NormalTexture:SetVertexColor(0.3, 0.3, 0.3)
-    end
+	end
 end
 
 hooksecurefunc("ActionButton_OnUpdate", TukuiActionBars.RangeOnUpdate)
