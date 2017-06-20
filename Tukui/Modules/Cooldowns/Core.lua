c968237b8168309fa8295fa9132fb800b47b8c3a
Tukz <tukz@tukui.org>
2017-06-20 11:29:37 -0400

    http://www.tukui.org/forums/topic.php?id=39628

--- Tukui/Modules/Cooldowns/Core.lua
+++ Tukui/Modules/Cooldowns/Core.lua
@@ -6,23 +6,25 @@ function CD:UpdateCooldown(start, duration, enable, charges, maxcharges, forceSh
 	local Enabled = GetCVar("countdownForCooldowns")
 
 	if (Enabled) then
-		if not self.IsCooldownTextEdited then
-			local NumRegions = self:GetNumRegions()
+		if not self:IsForbidden() then
+			if not self.IsCooldownTextEdited then
+				local NumRegions = self:GetNumRegions()
 
-			for i = 1, NumRegions do
-				local Region = select(i, self:GetRegions())
+				for i = 1, NumRegions do
+					local Region = select(i, self:GetRegions())
 
-				if Region.GetText then
-					local Font = T.GetFont(C["Cooldowns"].Font)
-					Font = _G[Font]:GetFont()
+					if Region.GetText then
+						local Font = T.GetFont(C["Cooldowns"].Font)
+						Font = _G[Font]:GetFont()
 
-					Region:SetFont(Font, 14, "OUTLINE")
-					Region:Point("CENTER", 1, 0)
-					Region:SetTextColor(1, 0, 0)
+						Region:SetFont(Font, 14, "OUTLINE")
+						Region:Point("CENTER", 1, 0)
+						Region:SetTextColor(1, 0, 0)
+					end
 				end
-			end
 
-			self.IsCooldownTextEdited = true
+				self.IsCooldownTextEdited = true
+			end
 		end
 	end
 end
