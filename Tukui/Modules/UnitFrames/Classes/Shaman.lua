e577a9b3dd17ba61928e6fa85ad33206561286b8
Tukz <tukz@tukui.org>
2018-07-07 22:04:34 -0400

    I forgot to register Movers in shaman.

--- Tukui/Modules/UnitFrames/Classes/Shaman.lua
+++ Tukui/Modules/UnitFrames/Classes/Shaman.lua
@@ -2,6 +2,7 @@ local T, C, L = select(2, ...):unpack()
 
 local TukuiUnitFrames = T["UnitFrames"]
 local Class = select(2, UnitClass("player"))
+local Movers = T["Movers"]
 
 if (Class ~= "SHAMAN") then
 	return
