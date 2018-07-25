4cff412af0f703df14288505e1d047aad9fefbfa
Tukz <tukz@tukui.org>
2018-07-25 13:24:58 -0400

    MISC: Fix Error Filter, also Filter is now a blacklist instead of a whitelist.

--- Tukui/Modules/Miscellaneous/ErrorFilter.lua
+++ Tukui/Modules/Miscellaneous/ErrorFilter.lua
@@ -3,13 +3,19 @@ local T, C, L = select(2, ...):unpack()
 local TukuiError = CreateFrame("Frame")
 local Miscellaneous = T["Miscellaneous"]
 
--- Set messages to allow
+-- Set messages to blacklist
 TukuiError.Filter = {
-	[INVENTORY_FULL] = true,
+	[ERR_NO_ATTACK_TARGET] = true,
+	[OUT_OF_ENERGY] = true,
+	[ERR_ABILITY_COOLDOWN] = true,
+	[SPELL_FAILED_NO_COMBO_POINTS] = true,
+	[SPELL_FAILED_SPELL_IN_PROGRESS] = true,
+	[ERR_SPELL_COOLDOWN] = true,
+	[SPELL_FAILED_BAD_TARGETS] = true,
 }
 
-function TukuiError:OnEvent(event, msg)
-	if self.Filter[msg] then
+function TukuiError:OnEvent(event, id, msg)
+	if not self.Filter[msg] then
 		UIErrorsFrame:AddMessage(msg, 1, 0, 0)
 	end
 end
