d64eda2fae512d9a2e1e0c309a0ea5715c1a38de
Tukz <tukz@tukui.org>
2018-10-09 17:45:03 -0400

    Another raid buffs/debuffs list update.

--- Tukui/Modules/UnitFrames/Tracking.lua
+++ Tukui/Modules/UnitFrames/Tracking.lua
@@ -3,14 +3,44 @@ local T, C, L = select(2, ...):unpack()
 local TukuiUnitFrames = T["UnitFrames"]
 
 ------------------------------------------------------------------------------------
--- RAID DEBUFFS (TRACKING LIST)
+-- Locales functions and tables
 ------------------------------------------------------------------------------------
-
-TukuiUnitFrames.DebuffsTracking = {}
 
 local function Defaults(priorityOverride)
 	return {["enable"] = true, ["priority"] = priorityOverride or 0, ["stackThreshold"] = 0}
 end
+
+local function ClassBuff(id, point, color, anyUnit, onlyShowMissing, style, displayText, decimalThreshold, textColor, textThreshold, xOffset, yOffset, sizeOverride)
+	local r, g, b = unpack(color)
+
+	local r2, g2, b2 = 1, 1, 1
+    
+	if textColor then
+		r2, g2, b2 = unpack(textColor)
+	end
+
+	return {
+        ["enabled"] = true, ["id"] = id, 
+        ["point"] = point, 
+        ["color"] = {["r"] = r, ["g"] = g, ["b"] = b},
+        ["anyUnit"] = anyUnit, 
+        ["onlyShowMissing"] = onlyShowMissing, 
+        ['style'] = style or 'coloredIcon', 
+        ['displayText'] = displayText or false, 
+        ['decimalThreshold'] = decimalThreshold or 5,
+        ['textColor'] = {["r"] = r2, ["g"] = g2, ["b"] = b2},
+        ['textThreshold'] = textThreshold or -1,
+        ['xOffset'] = xOffset or 0,
+        ['yOffset'] = yOffset or 0,
+        ["sizeOverride"] = sizeOverride or 0
+    }
+end
+
+TukuiUnitFrames.DebuffsTracking = {}
+
+------------------------------------------------------------------------------------
+-- RAID DEBUFFS (TRACKING LIST)
+------------------------------------------------------------------------------------
 
 TukuiUnitFrames.DebuffsTracking["RaidDebuffs"] = {
 	["type"] = "Whitelist",
@@ -259,9 +289,9 @@ TukuiUnitFrames.DebuffsTracking["RaidDebuffs"] = {
 	},
 }
 
---------------------------------
--- CC DEBUFFS (TRACKING LIST) --
---------------------------------
+------------------------------------------------------------------------------------
+-- CC DEBUFFS (TRACKING LIST)
+------------------------------------------------------------------------------------
 
 TukuiUnitFrames.DebuffsTracking["CCDebuffs"] = {
 	["type"] = "Whitelist",
@@ -439,41 +469,75 @@ TukuiUnitFrames.DebuffsTracking["CCDebuffs"] = {
 
 TukuiUnitFrames.RaidBuffsTracking = {
 	PRIEST = {
-		{41635, "BOTTOMRIGHT", {0.2, 0.7, 0.2}},             -- Prayer of Mending 
-		{139, "BOTTOMLEFT", {0.4, 0.7, 0.2}},                -- Renew
-		{17, "TOPLEFT", {1, 1, 0}, true},                    -- Power Word: Shield
-		{194384, "TOPRIGHT", {0.81, 0.70, 0.23}, true},      -- Atonement
-		{214206, "TOPRIGHT", {0.81, 0.70, 0.23}, true},      -- PVP Atonement
+		[194384] = ClassBuff(194384, "TOPRIGHT", {1, 1, 0.66}),                -- Atonement
+		[214206] = ClassBuff(214206, "TOPRIGHT", {1, 1, 0.66}),                -- Atonement (PvP)
+		[41635]  = ClassBuff(41635, "BOTTOMRIGHT", {0.2, 0.7, 0.2}),           -- Prayer of Mending
+		[193065] = ClassBuff(193065, "BOTTOMRIGHT", {0.54, 0.21, 0.78}),       -- Masochism
+		[139]    = ClassBuff(139, "BOTTOMLEFT", {0.4, 0.7, 0.2}),              -- Renew
+		[17]     = ClassBuff(17, "TOPLEFT", {0.7, 0.7, 0.7}, true),            -- Power Word: Shield
+		[47788]  = ClassBuff(47788, "LEFT", {0.86, 0.45, 0}, true),            -- Guardian Spirit
+		[33206]  = ClassBuff(33206, "LEFT", {0.47, 0.35, 0.74}, true),         -- Pain Suppression
 	},
-
+    
 	DRUID = {
-		{774, "TOPLEFT", {0.8, 0.4, 0.8}},                   -- Rejuvenation
-		{155777, "LEFT", {0.8, 0.4, 0.8}},                   -- Germination
-		{8936, "TOPRIGHT", {0.2, 0.8, 0.2}},                 -- Regrowth
-		{33763, "BOTTOMLEFT", {0.4, 0.8, 0.2}},              -- Lifebloom
-		{48438, "BOTTOMRIGHT", {0.8, 0.4, 0}},               -- Wild Growth
+		[774]    = ClassBuff(774, "TOPRIGHT", {0.8, 0.4, 0.8}),   		      -- Rejuvenation
+		[155777] = ClassBuff(155777, "RIGHT", {0.8, 0.4, 0.8}),   		      -- Germination
+		[8936]   = ClassBuff(8936, "BOTTOMLEFT", {0.2, 0.8, 0.2}),		      -- Regrowth
+		[33763]  = ClassBuff(33763, "TOPLEFT", {0.4, 0.8, 0.2}),  		      -- Lifebloom
+		[48438]  = ClassBuff(48438, "BOTTOMRIGHT", {0.8, 0.4, 0}),		      -- Wild Growth
+		[207386] = ClassBuff(207386, "TOP", {0.4, 0.2, 0.8}),     		      -- Spring Blossoms
+		[102351] = ClassBuff(102351, "LEFT", {0.2, 0.8, 0.8}),    		      -- Cenarion Ward (Initial Buff)
+		[102352] = ClassBuff(102352, "LEFT", {0.2, 0.8, 0.8}),    		      -- Cenarion Ward (HoT)
+		[200389] = ClassBuff(200389, "BOTTOM", {1, 1, 0.4}),      		      -- Cultivation
 	},
-
+    
 	PALADIN = {
-		{53563, "TOPLEFT", {0.7, 0.3, 0.7}},	             -- Beacon of Light
-		{156910, "TOPRIGHT", {0.7, 0.3, 0.7}},	             -- Beacon of Faith
-		{1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true}, 	     -- Hand of Protection
-		{1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true},	     -- Hand of Freedom
-		{6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true},	     -- Hand of Sacrifice
+		[53563]  = ClassBuff(53563, "TOPRIGHT", {0.7, 0.3, 0.7}),             -- Beacon of Light
+		[156910] = ClassBuff(156910, "TOPRIGHT", {0.7, 0.3, 0.7}),            -- Beacon of Faith
+		[200025] = ClassBuff(200025, "TOPRIGHT", {0.7, 0.3, 0.7}),            -- Beacon of Virtue
+		[1022]   = ClassBuff(1022, "BOTTOMRIGHT", {0.2, 0.2, 1}, true),       -- Hand of Protection
+		[1044]   = ClassBuff(1044, "BOTTOMRIGHT", {0.89, 0.45, 0}, true),     -- Hand of Freedom
+		[6940]   = ClassBuff(6940, "BOTTOMRIGHT", {0.89, 0.1, 0.1}, true),    -- Hand of Sacrifice
+		[223306] = ClassBuff(223306, 'BOTTOMLEFT', {0.7, 0.7, 0.3}),          -- Bestow Faith
 	},
-
+    
 	SHAMAN = {
-		{61295, "TOPLEFT", {0.7, 0.3, 0.7}},                 -- Riptide
+		[61295]  = ClassBuff(61295, "TOPRIGHT", {0.7, 0.3, 0.7}),   	      -- Riptide
+		[974]    = ClassBuff(974, "BOTTOMRIGHT", {0.2, 0.2, 1}), 	          -- Earth Shield
 	},
-
+    
 	MONK = {
-		{119611, "TOPLEFT", {0.8, 0.4, 0.8}},	             -- Renewing Mist
-		{116849, "TOPRIGHT", {0.2, 0.8, 0.2}},	             -- Life Cocoon
-		{124682, "BOTTOMLEFT", {0.4, 0.8, 0.2}},             -- Enveloping Mist
-		{124081, "BOTTOMRIGHT", {0.7, 0.4, 0}},              -- Zen Sphere
+		[119611] = ClassBuff(119611, "TOPLEFT", {0.3, 0.8, 0.6}),             -- Renewing Mist
+		[116849] = ClassBuff(116849, "TOPRIGHT", {0.2, 0.8, 0.2}, true),      -- Life Cocoon
+		[124682] = ClassBuff(124682, "BOTTOMLEFT", {0.8, 0.8, 0.25}),         -- Enveloping Mist
+		[191840] = ClassBuff(191840, "BOTTOMRIGHT", {0.27, 0.62, 0.7}),       -- Essence Font
 	},
-
-	ALL = {
-		{14253, "RIGHT", {0, 1, 0}},                         -- Abolish Poison
+    
+	ROGUE = {
+		[57934]  = ClassBuff(57934, "TOPRIGHT", {0.89, 0.09, 0.05}),		  -- Tricks of the Trade
 	},
+    
+	WARRIOR = {
+		[114030] = ClassBuff(114030, "TOPLEFT", {0.2, 0.2, 1}),     	      -- Vigilance
+		[3411]   = ClassBuff(3411, "TOPRIGHT", {0.89, 0.09, 0.05}), 	      -- Intervene
+	},
+    
+	PET = {
+		-- Warlock Pets
+		[193396] = ClassBuff(193396, 'TOPRIGHT', {0.6, 0.2, 0.8}, true),      -- Demonic Empowerment
+        
+		-- Hunter Pets
+		[272790] = ClassBuff(272790, 'TOPLEFT', {0.89, 0.09, 0.05}, true),    -- Frenzy
+		[136]    = ClassBuff(136, 'TOPRIGHT', {0.2, 0.8, 0.2}, true)          -- Mend Pet
+	},
+    
+	HUNTER = {},
+    
+	DEMONHUNTER = {},
+    
+	WARLOCK = {},
+    
+	MAGE = {},
+    
+	DEATHKNIGHT = {},
 }
