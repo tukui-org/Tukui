fbd2f38a0d917c181974e1cdfcc589c8c1ed27f6
Tukz <tukz@tukui.org>
2018-10-11 12:46:26 -0400

    Wrong spellid for intervene.

--- Tukui/Modules/UnitFrames/Tracking.lua
+++ Tukui/Modules/UnitFrames/Tracking.lua
@@ -493,6 +493,6 @@ TukuiUnitFrames.RaidBuffsTracking = {
     
 	WARRIOR = {
 		{114030, "TOPLEFT", {0.2, 0.2, 1}},     	      -- Vigilance
-		{3411, "TOPRIGHT", {0.89, 0.09, 0.05}}, 	      -- Intervene
+		{122506, "TOPRIGHT", {0.89, 0.09, 0.05}}, 	      -- Intervene
 	},
 }
