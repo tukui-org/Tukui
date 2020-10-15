77d82733e94683cbe560e89f6aa921be3a3c99f3
Tukz <tukz@tukui.org>
2020-10-15 10:52:06 -0400

    Add AdditionalPower for Druid Balance and Shadow Priest

--- Tukui/Modules/UnitFrames/Classes/Druid.lua
+++ Tukui/Modules/UnitFrames/Classes/Druid.lua
@@ -8,5 +8,23 @@ if (Class ~= "DRUID") then
 end
 
 UnitFrames.AddClassFeatures["DRUID"] = function(self)
+	local HealthTexture = T.GetTexture(C["Textures"].UFHealthTexture)
+	local AdditionalPower = CreateFrame("StatusBar", self:GetName().."AdditionalPower", self.Health)
+	
+	AdditionalPower:SetHeight(6)
+	AdditionalPower:SetPoint("BOTTOMLEFT", self.Health, "BOTTOMLEFT")
+	AdditionalPower:SetPoint("BOTTOMRIGHT", self.Health, "BOTTOMRIGHT")
+	AdditionalPower:SetStatusBarTexture(HealthTexture)
+	AdditionalPower:SetFrameLevel(self.Health:GetFrameLevel() + 1)
+	AdditionalPower:CreateBackdrop()
+	AdditionalPower:SetStatusBarColor(unpack(T.Colors.power.MANA))
+	AdditionalPower.Backdrop:SetOutside()
 
+	AdditionalPower.Background = AdditionalPower:CreateTexture(nil, "ARTWORK")
+	AdditionalPower.Background:SetAllPoints(AdditionalPower)
+	AdditionalPower.Background:SetTexture(HealthTexture)
+	AdditionalPower.Background:SetColorTexture(T.Colors.power.MANA[1], T.Colors.power.MANA[2], T.Colors.power.MANA[3], C.UnitFrames.StatusBarBackgroundMultiplier / 100)
+
+	-- Register it with oUF
+	self.AdditionalPower = AdditionalPower
 end
