ceaf752c66d30acdd407a52aad093556c1a4f9b6
Tukz <tukz@tukui.org>
2020-10-16 15:04:08 -0400

    Update time datatext

--- Tukui/Modules/DataTexts/Elements/Time.lua
+++ Tukui/Modules/DataTexts/Elements/Time.lua
@@ -19,12 +19,69 @@ local Update = function(self, Elapsed)
 	end
 end
 
+local OnEnter = function(self)
+	GameTooltip:SetOwner(self:GetTooltipAnchor())
+	GameTooltip:ClearLines()
+
+	local SavedInstances = GetNumSavedInstances()
+	local SavedWorldBosses = GetNumSavedWorldBosses()
+
+	if (SavedWorldBosses > 0) then
+		GameTooltip:AddLine("World Bosses")
+
+		for i = 1, SavedWorldBosses do
+			local Name, _, Reset = GetSavedWorldBossInfo(i)
+
+			if (Name and Reset) then
+				local ResetTime = GetResetTime(Reset)
+
+				GameTooltip:AddDoubleLine(Name, ResetTime, 1, 1, 1, 1, 1, 1)
+			end
+		end
+	end
+
+	if ((SavedWorldBosses > 0) and (SavedInstances > 0)) then
+		-- Spacing
+		GameTooltip:AddLine(" ")
+	end
+
+	if (SavedInstances > 0) then
+		GameTooltip:AddLine("Saved Raids")
+
+		for i = 1, SavedInstances do
+			local Name, _, Reset, _, Locked, Extended, _, IsRaid, _, Difficulty, MaxBosses, DefeatedBosses = GetSavedInstanceInfo(i)
+
+			if (IsRaid and Name and (Locked or Extended)) then
+				local ResetTime = GetResetTime(Reset)
+
+				if (MaxBosses and MaxBosses > 0) and (DefeatedBosses and DefeatedBosses > 0) then
+					GameTooltip:AddDoubleLine(format(RaidFormat1, Name, Difficulty, DefeatedBosses, MaxBosses), ResetTime, 1, 1, 1, 1, 1, 1)
+				else
+					GameTooltip:AddDoubleLine(format(RaidFormat2, Name, Difficulty), ResetTime, 1, 1, 1, 1, 1, 1)
+				end
+			end
+		end
+	end
+
+	GameTooltip:Show()
+end
+
+local OnLeave = function()
+	GameTooltip:Hide()
+end
+
 local Enable = function(self)
 	self:SetScript("OnUpdate", Update)
+	self:SetScript("OnEnter", OnEnter)
+	self:SetScript("OnLeave", OnLeave)
+	self:SetScript("OnMouseUp", GameTimeFrame_OnClick)
 end
 
 local Disable = function(self)
 	self:SetScript("OnUpdate", nil)
+	self:SetScript("OnEnter", nil)
+	self:SetScript("OnLeave", nil)
+	self:SetScript("OnMouseUp", nil)
 end
 
 DataText:Register("Time", Enable, Disable, Update)
