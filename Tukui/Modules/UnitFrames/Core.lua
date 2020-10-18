d21e7ae4f30c53c777bb395919ba0c290fce68c5
Tukz <tukz@tukui.org>
2020-10-18 12:16:53 -0400

    Flash buffs on enemy that can be dispel

--- Tukui/Modules/UnitFrames/Core.lua
+++ Tukui/Modules/UnitFrames/Core.lua
@@ -422,7 +422,7 @@ function UnitFrames:PostUpdateAura(unit, button, index, offset, filter, isDebuff
 		else
 			-- These classes can purge, show them
 			if (button.Animation) and (T.MyClass == "PRIEST") or (T.MyClass == "SHAMAN") then
-				if (DType == "Magic") and (not UnitIsFriend("player", unit)) and (not button.Animation.Playing) then
+				if (IsStealable or DType == "Magic") and not UnitIsFriend("player", unit) and not button.Animation.Playing then
 					button.Animation:Play()
 					button.Animation.Playing = true
 				else
