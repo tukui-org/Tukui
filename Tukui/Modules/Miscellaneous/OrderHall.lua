f5563cd3fd50711a9734f024467bc5f424cbe474
Tukz <tukz@tukui.org>
2020-10-19 18:01:35 -0400

    Update OrderHall.lua

--- Tukui/Modules/Miscellaneous/OrderHall.lua
+++ Tukui/Modules/Miscellaneous/OrderHall.lua
@@ -1,2 +1,24 @@
 local T, C, L = select(2, ...):unpack()
 
+local Miscellaneous = T["Miscellaneous"]
+local OrderHall = CreateFrame("Frame")
+
+function OrderHall:OnEvent(event, addon)
+	if addon ~= "Blizzard_OrderHallUI" then
+		return
+	end
+
+	local Frame = OrderHallCommandBar
+
+	Frame:Kill()
+
+	self:UnregisterAllEvents()
+end
+
+function OrderHall:Enable()
+	self:RegisterEvent("ADDON_LOADED")
+	self:SetScript("OnEvent", self.OnEvent)
+end
+
+Miscellaneous.OrderHall = OrderHall
+
