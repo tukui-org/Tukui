ace72e2e96402c3153e414edf5876947d70a5285
Tukz <tukz@tukui.org>
2020-10-23 15:11:50 -0400

    Optimize nameplate quest icon scanning

--- Tukui/Libs/oUF_QuestIcon/oUF_QuestIcon.lua
+++ Tukui/Libs/oUF_QuestIcon/oUF_QuestIcon.lua
@@ -2,47 +2,71 @@ local parent, ns = ...
 local oUF = ns.oUF
 local ScanTooltip = CreateFrame("GameTooltip", "oUF_QuestIconTooltip", UIParent, "GameTooltipTemplate")
 
+Cache = {
+	-- Cache for NPCs
+}
+
+local ClearCache = function(self)
+	Cache = {}
+end
+
+local DisplayQuestIcon = function(self)
+	local QuestIcon = self.QuestIcon
+	local Name = UnitName(self.unit)
+	
+	if Cache[Name] == "QUEST" then
+		if not QuestIcon:IsShown() then
+			QuestIcon:Show()
+		end
+	else
+		if QuestIcon:IsShown() then
+			QuestIcon:Hide()
+		end
+	end
+end
+
 local FindPlateWithQuest = function(self, unit)
 	local QuestIcon = self.QuestIcon
 	
 	if QuestIcon then
-		ScanTooltip:ClearLines()
-		ScanTooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
-		ScanTooltip:SetUnit(unit)
-		ScanTooltip:Show()
+		local Name = UnitName(unit)
+		
+		if not Cache[Name] then
+			ScanTooltip:ClearLines()
+			ScanTooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
+			ScanTooltip:SetUnit(unit)
+			ScanTooltip:Show()
 
-		local NumLines = ScanTooltip:NumLines()
+			local NumLines = ScanTooltip:NumLines()
+			local Name = UnitName(unit)
+			
+			Cache[Name] = "NOQUEST"
 
-		if NumLines >= 3 then
-			for i = 3, NumLines do
-				local Line = _G[ScanTooltip:GetName().."TextLeft"..i]
-				local r, g, b = Line:GetTextColor()
+			if NumLines >= 3 then
+				for i = 3, NumLines do
+					local Line = _G[ScanTooltip:GetName().."TextLeft"..i]
+					local r, g, b = Line:GetTextColor()
 
-				if (r > 0.99 and r <= 1) and (g > 0.82 and g < 0.83) and (b >= 0 and b < 0.01) then
-					if not QuestIcon:IsShown() then
-						QuestIcon:Show()
-					end
-
-					break
-				else
-					if QuestIcon:IsShown() then
-						QuestIcon:Hide()
+					if (r > 0.99 and r <= 1) and (g > 0.82 and g < 0.83) and (b >= 0 and b < 0.01) then
+						Cache[Name] = "QUEST"
+						
+						break
 					end
 				end
 			end
-		else
-			if QuestIcon:IsShown() then
-				QuestIcon:Hide()
-			end
+			
+			ScanTooltip:Hide()
 		end
-
-		ScanTooltip:Hide()
 	end
 end
 
 local Update = function(self, event)
 	if IsInInstance() then
 		return
+	end
+	
+	if event ~= "NAME_PLATE_UNIT_ADDED" then
+		ClearCache()
 	end
 	
 	local QuestIcon = self.QuestIcon
@@ -55,6 +79,8 @@ local Update = function(self, event)
 	
 	for i, Plate in pairs(NumPlates) do
 		FindPlateWithQuest(self, Unit)
+		
+		DisplayQuestIcon(self)
 	end
 
 	if(QuestIcon.PostUpdate) then
@@ -82,7 +108,8 @@ local function Enable(self)
 			QuestIcon:SetTexCoord(0.13476563, 0.17187500, 0.01562500, 0.53125000)
 		end
 
-		self:RegisterEvent("QUEST_LOG_UPDATE", Path, true)
+		self:RegisterEvent("QUEST_ACCEPTED", Path, true)
+		self:RegisterEvent("QUEST_REMOVED", Path, true)
 		self:RegisterEvent("NAME_PLATE_UNIT_ADDED", Path, true)
 
 		return true
@@ -93,7 +120,8 @@ local function Disable(self)
 	local QuestIcon = self.QuestIcon
 	
 	if (QuestIcon) then
-		self:UnregisterEvent('QUEST_LOG_UPDATE', Path, true)
+		self:UnregisterEvent('QUEST_ACCEPTED', Path, true)
+		self:UnregisterEvent('QUEST_REMOVED', Path, true)
 		self:UnregisterEvent('NAME_PLATE_UNIT_ADDED', Path, true)
 	end
 end
