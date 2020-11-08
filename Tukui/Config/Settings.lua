6902e28b10c5ddd20501184ddc0f88fff57268f9
Tukz <tukz@tukui.org>
2020-11-08 12:20:20 -0500

    Missing HP tag should be in red

--- Tukui/Config/Settings.lua
+++ Tukui/Config/Settings.lua
@@ -296,7 +296,7 @@ C["UnitFrames"] = {
 			["Current HP"] = "|cff549654[Tukui:CurrentHP]|r",
 			["Percent"] = "|cff549654[perhp]%|r",
 			["HP and Percent"] = "|cff549654[Tukui:CurrentHP] - [perhp]%|r",
-			["Missing HP"] = "|cff549654-[missinghp]|r",
+			["Missing HP"] = "|cffFF0000[missinghp]|r",
 		},
 
 		["Value"] = "|cff549654[perhp]%|r",
@@ -307,7 +307,7 @@ C["UnitFrames"] = {
 			["Current HP"] = "|cff549654[Tukui:CurrentHP]|r",
 			["Percent"] = "|cff549654[perhp]%|r",
 			["HP and Percent"] = "|cff549654[Tukui:CurrentHP] - [perhp]%|r",
-			["Missing HP"] = "|cff549654-[missinghp]|r",
+			["Missing HP"] = "|cffFF0000[missinghp]|r",
 		},
 
 		["Value"] = "|cff549654[perhp]%|r",
@@ -318,7 +318,7 @@ C["UnitFrames"] = {
 			["Current HP"] = "|cff549654[Tukui:CurrentHP]|r",
 			["Percent"] = "|cff549654[perhp]%|r",
 			["HP and Percent"] = "|cff549654[Tukui:CurrentHP] - [perhp]%|r",
-			["Missing HP"] = "|cff549654-[missinghp]|r",
+			["Missing HP"] = "|cffFF0000[missinghp]|r",
 		},
 
 		["Value"] = "|cff549654[perhp]%|r",
