d62bbf48b38efef434e15616105c34741257140e
Tukz <tukz@tukui.org>
2020-12-07 14:28:01 -0500

    Don't move back UIWidget under minimap

--- Tukui/Modules/Miscellaneous/UIWidgets.lua
+++ Tukui/Modules/Miscellaneous/UIWidgets.lua
@@ -7,7 +7,7 @@ function UIWidgets:Enable()
 	local MinimapWidget = UIWidgetBelowMinimapContainerFrame
 
 	-- Hack to avoid UIWidgetBelowMinimapContainerFrame to move in UIParent.lua (L2987)
-	MinimapWidget.GetHeight = function() return 0 end
+	MinimapWidget.GetNumWidgetsShowing = function() return 0 end
 
 	-- This is now the frame that contain capture bar and other shit like that.
 	MinimapWidget:ClearAllPoints()
