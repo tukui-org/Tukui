163b80acdf6f80d5ac83997a5255220aaf9c53d5
Tukz <tukz@tukui.org>
2020-12-10 17:34:20 -0500

    Update UIWidgets.lua

--- Tukui/Modules/Miscellaneous/UIWidgets.lua
+++ Tukui/Modules/Miscellaneous/UIWidgets.lua
@@ -12,6 +12,9 @@ function UIWidgets:Enable()
 	-- This is now the frame that contain capture bar and other shit like that.
 	MinimapWidget:ClearAllPoints()
 	MinimapWidget:SetPoint("TOP", 3, -66)
+	MinimapWidget:SetSize(220, 20)
+	
+	T.Movers:RegisterFrame(MinimapWidget)
 end
 
 Miscellaneous.UIWidgets = UIWidgets
