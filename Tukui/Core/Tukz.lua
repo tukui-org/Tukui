7088d6f24383a15441f748622fc3f796645571f4
Tukz <tukz@tukui.org>
2020-12-14 18:34:33 -0500

    Update my own script

--- Tukui/Core/Tukz.lua
+++ Tukui/Core/Tukz.lua
@@ -4,6 +4,34 @@ local T, C, L = select(2, ...):unpack()
 
 local Tukz = CreateFrame("Frame")
 
+Tukz:RegisterEvent("PLAYER_PVP_TALENT_UPDATE")
+Tukz:RegisterEvent("ADDON_LOADED")
+Tukz:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+function Tukz:OnEvent(event, arg)
+	if event == "ADDON_LOADED" and arg == "Tukui" then
+		if IsAddOnLoaded("OmniBar") then
+			hooksecurefunc("OmniBar_AddIcon", self.SkinOmniBarButton)
+			hooksecurefunc("OmniBar_ShowAnchor", self.ToggleOmniBarAnchor)
+			hooksecurefunc("OmniBar_LoadPosition", self.DefaultOmniBarSettings)
+		end
+	end
+	
+	for Macro = 1, MAX_ACCOUNT_MACROS + MAX_CHARACTER_MACROS do
+		local Name, Icon, Body = GetMacroInfo(Macro)
+		local Spec = GetSpecialization()
+		local Class = select(2, UnitClass("player"))
+		
+		if Class == "PRIEST" and Spec == 1 then
+			if Name and string.find(Body, "Archangel") then
+				SetMacroSpell(Name, GetSpellInfo("Archangel(PvP Talent)") or GetSpellInfo("Thoughtsteal") or GetSpellInfo("Dark Archangel(PvP Talent)") or GetSpellInfo("Atonement"))
+				
+				break
+			end
+		end
+	end
+end
+
 function Tukz:SkinOmniBarButton()
 	for i = 1, #self.active do
 		if self.active[i] then
@@ -38,14 +66,6 @@ function Tukz:DefaultOmniBarSettings()
 	self:SetPoint("CENTER", UIParent, "CENTER", 0, -200)
 end
 
-function Tukz:Enable()
-	if IsAddOnLoaded("OmniBar") then
-		hooksecurefunc("OmniBar_AddIcon", self.SkinOmniBarButton)
-		hooksecurefunc("OmniBar_ShowAnchor", self.ToggleOmniBarAnchor)
-		hooksecurefunc("OmniBar_LoadPosition", self.DefaultOmniBarSettings)
-	end
-end
-
 if T.MyName == "Tukz" and T.MyRealm == "Illidan" and GetCurrentRegion() == 3 then
-	Tukz:Enable()
+	Tukz:SetScript("OnEvent", Tukz.OnEvent)
 end
