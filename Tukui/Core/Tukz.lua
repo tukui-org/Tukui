22da9d2e1ceac07c1e6733ea5220f9e2eff0d081
Tukz <tukz@tukui.org>
2020-12-15 22:10:06 -0500

    Cleanup

--- Tukui/Core/Tukz.lua
+++ Tukui/Core/Tukz.lua
@@ -4,30 +4,12 @@ local T, C, L = select(2, ...):unpack()
 
 local Tukz = CreateFrame("Frame")
 
-Tukz:RegisterEvent("PLAYER_PVP_TALENT_UPDATE")
-Tukz:RegisterEvent("ADDON_LOADED")
-Tukz:RegisterEvent("PLAYER_ENTERING_WORLD")
-
 function Tukz:OnEvent(event, arg)
 	if event == "ADDON_LOADED" and arg == "Tukui" then
 		if IsAddOnLoaded("OmniBar") then
 			hooksecurefunc("OmniBar_AddIcon", self.SkinOmniBarButton)
 			hooksecurefunc("OmniBar_ShowAnchor", self.ToggleOmniBarAnchor)
 			hooksecurefunc("OmniBar_LoadPosition", self.DefaultOmniBarSettings)
-		end
-	end
-	
-	for Macro = 1, MAX_ACCOUNT_MACROS + MAX_CHARACTER_MACROS do
-		local Name, Icon, Body = GetMacroInfo(Macro)
-		local Spec = GetSpecialization()
-		local Class = select(2, UnitClass("player"))
-		
-		if Class == "PRIEST" and Spec == 1 then
-			if Name and string.find(Body, "Archangel") then
-				SetMacroSpell(Name, GetSpellInfo("Archangel(PvP Talent)") or GetSpellInfo("Thoughtsteal") or GetSpellInfo("Dark Archangel(PvP Talent)") or GetSpellInfo("Atonement"))
-				
-				break
-			end
 		end
 	end
 end
@@ -67,5 +49,6 @@ function Tukz:DefaultOmniBarSettings()
 end
 
 if T.MyName == "Tukz" and T.MyRealm == "Illidan" and GetCurrentRegion() == 3 then
+	Tukz:RegisterEvent("ADDON_LOADED")
 	Tukz:SetScript("OnEvent", Tukz.OnEvent)
 end
