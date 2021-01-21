012d99a4de64a762619c70c8d35231f608a871de
Tukz <tukz@tukui.org>
2021-01-21 04:29:01 -0500

    Lua error while casting

--- Tukui/Modules/UnitFrames/Core.lua
+++ Tukui/Modules/UnitFrames/Core.lua
@@ -240,7 +240,7 @@ function UnitFrames:CheckInterrupt(unit)
 	if (self.notInterruptible) then
 		self:SetStatusBarColor(0.85, 0.09, 0.09, 1)
 		
-		if C.NamePlates.ClassIcon and self.Button.Shadow and unit:find("nameplate") then
+		if C.NamePlates.ClassIcon and unit:find("nameplate") and self.Button and self.Button.Shadow then
 			self.Button.Shadow:SetBackdropBorderColor(0.85, 0.09, 0.09, 1)
 		end
 	else
