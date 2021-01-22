d49954df79bd57759d251c1764066f7dd852a359
Tukz <tukz@tukui.org>
2021-01-22 05:38:25 -0500

    Let's UI Widgets have his own holder, because the frame is a 1x1 pixel frame

--- Tukui/Modules/Miscellaneous/UIWidgets.lua
+++ Tukui/Modules/Miscellaneous/UIWidgets.lua
@@ -9,12 +9,17 @@ function UIWidgets:Enable()
 	-- Hack to avoid UIWidgetBelowMinimapContainerFrame to move in UIParent.lua (L2987)
 	MinimapWidget.GetNumWidgetsShowing = function() return 0 end
 
+	-- Create a widget holder
+	local Holder = CreateFrame("Frame", "TukuiWidget", UIParent)
+	Holder:SetSize(220, 20)
+	Holder:SetPoint("TOP", 3, -96)
+
 	-- This is now the frame that contain capture bar and other shit like that.
+	MinimapWidget:SetParent(Holder)
 	MinimapWidget:ClearAllPoints()
-	MinimapWidget:SetPoint("TOP", 3, -96)
-	MinimapWidget:SetSize(220, 20)
+	MinimapWidget:SetPoint("CENTER")
 	
-	T.Movers:RegisterFrame(MinimapWidget, "UI Widgets")
+	T.Movers:RegisterFrame(Holder, "UI Widgets")
 end
 
 Miscellaneous.UIWidgets = UIWidgets
