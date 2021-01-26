074fda6fe764a0f3482e5e6b90b0df292c218c36
Tukz <tukz@tukui.org>
2021-01-26 12:32:15 -0500

    Alerts can now be moved

---
+++ Tukui/Modules/Miscellaneous/Alerts.lua
@@ -0,0 +1,29 @@
+local T, C, L = select(2, ...):unpack()
+
+local Miscellaneous = T["Miscellaneous"]
+local Movers = T["Movers"]
+local Alerts = CreateFrame("Frame")
+
+function Alerts:UpdateAnchors()
+	AlertFrame:ClearAllPoints()
+	AlertFrame:SetPoint("CENTER", Alerts.Holder, "Center", 0, 0)
+end
+
+function Alerts:AddHooks()
+	hooksecurefunc(AlertFrame, "UpdateAnchors", self.UpdateAnchors)
+end
+
+function Alerts:AddHolder()
+	self.Holder = CreateFrame("Frame", "TukuiAlerts", UIParent)
+	self.Holder:SetSize(200, 17)
+	self.Holder:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 336)
+end
+
+function Alerts:Enable()
+	self:AddHolder()
+	self:AddHooks()
+	
+	Movers:RegisterFrame(self.Holder, "Alerts holder")
+end
+
+Miscellaneous.Alerts = Alerts
