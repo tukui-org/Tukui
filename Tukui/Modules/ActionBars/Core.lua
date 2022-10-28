c5a7695d8c22fc954f01e64fe36e0fd2c1b8880c
Tukz <tukz@tukui.org>
2022-10-28 14:44:40 -0400

    ActionButtonUseKeyDown to 0, always.

--- Tukui/Modules/ActionBars/Core.lua
+++ Tukui/Modules/ActionBars/Core.lua
@@ -486,6 +486,7 @@ function ActionBars:Enable()
 	end
 
 	SetCVar("alwaysShowActionBars", 1)
+	SetCVar("ActionButtonUseKeyDown", 0)
 	SetActionBarToggles(1, 1, 1, 1, 1, 1, 1)
 
 	self:DisableBlizzard()
