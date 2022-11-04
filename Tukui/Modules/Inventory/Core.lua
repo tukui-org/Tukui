254a0455224dc7d2020fca60f9277870bf1f7119
Tukz <tukz@tukui.org>
2022-11-04 12:25:32 -0400

    This need to be fixed on DF Beta current build.

--- Tukui/Modules/Inventory/Core.lua
+++ Tukui/Modules/Inventory/Core.lua
@@ -3,7 +3,11 @@ local T, C, L = select(2, ...):unpack()
 local Inventory = T["Inventory"]
 
 function Inventory:Enable()
-	Inventory["Bags"]:Enable()
+	-- FIX ME ON BETA
+	if T.TocVersion < 100002 then
+		Inventory["Bags"]:Enable()
+	end
+	
 	Inventory["Loot"]:Enable()
 	Inventory["GroupLoot"]:Enable()
 	Inventory["Merchant"]:Enable()
