209927806b818459015c32d4071a05003e875cc6
Tukz <tukz@tukui.org>
2023-01-19 13:17:59 -0500

    Fix auto-sell junk on WOTLK.

--- Tukui/Modules/Inventory/Merchant.lua
+++ Tukui/Modules/Inventory/Merchant.lua
@@ -23,7 +23,7 @@ function Merchant:SellJunk()
 				local Mult1 = select(11, GetItemInfo(Link))
 				local Mult2
 				
-				if T.Retail then
+				if T.Retail or T.WotLK then
 					local Table = GetContainerItemInfo(Bag, Slot)
 					
 					if Table and Table.stackCount then
