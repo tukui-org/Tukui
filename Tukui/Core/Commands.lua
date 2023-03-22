396d5b05a7de0a60c5186e3870887f953bb9681a
Tukz <tukz@tukui.org>
2023-03-21 21:11:40 -0400

    Fix /tukui mm

--- Tukui/Core/Commands.lua
+++ Tukui/Core/Commands.lua
@@ -45,31 +45,7 @@ T.SlashHandler = function(cmd)
 	elseif (arg1 == "mm") or (arg1 == "micromenu") then
 		local MicroMenu = T.Miscellaneous.MicroMenu
 
-		if MicroMenu:IsShown() then
-			MicroMenu:Hide()
-
-			UpdateMicroButtonsParent(T.Hider)
-
-			for i = 1, #MICRO_BUTTONS do
-				local Button = _G[MICRO_BUTTONS[i]]
-
-				if Button.Backdrop then
-					Button.Backdrop:Hide()
-				end
-			end
-		else
-			MicroMenu:Show()
-
-			for i = 1, #MICRO_BUTTONS do
-				local Button = _G[MICRO_BUTTONS[i]]
-
-				if Button.Backdrop then
-					Button.Backdrop:Show()
-				end
-			end
-
-			UpdateMicroButtonsParent(T.PetHider)
-		end
+		MicroMenu:Toggle()
 	elseif (arg1 == "ot") or (arg1 == "quests") then
 		if T.Retail then
 			if (ObjectiveTrackerFrame:IsVisible()) then
