08a9962e3087242d389dd00758c3d7bec66e25f3
pedrozc90 <pedrozc90@gmail.com>
2023-07-12 14:01:41 -0300

    update MirrorTimers.lua

--- Tukui/Modules/Miscellaneous/MirrorTimers.lua
+++ Tukui/Modules/Miscellaneous/MirrorTimers.lua
@@ -3,37 +3,41 @@ local T, C, L = unpack((select(2, ...)))
 local Miscellaneous = T["Miscellaneous"]
 local MirrorTimers = CreateFrame("Frame")
 
-function MirrorTimers:Update()
-	for i = 1, MIRRORTIMER_NUMTIMERS, 1 do
-		local Bar = _G["MirrorTimer"..i]
-		
-		if Bar and not Bar.isSkinned then
-			local Status = Bar.StatusBar or _G[Bar:GetName().."StatusBar"]
-			local Border = Bar.Border or _G[Bar:GetName().."Border"]
-			local Text = Bar.Text or _G[Bar:GetName().."Text"]
+local MirrorTimerContainer = _G.MirrorTimerContainer
+local MirrorTimerAtlas = _G.MirrorTimerAtlas
 
-			Bar:StripTextures()
-			Bar:CreateBackdrop()
-			Bar.Backdrop:SetBackdropColor(unpack(C.General.BackdropColor))
-			Bar.Backdrop:CreateShadow()
+function MirrorTimers:SetupTimer(timer, value, maxvalue, paused, label)
+	local Bar = MirrorTimerContainer:GetAvailableTimer(timer);
+	if (not Bar) then return end
 
-			Status:ClearAllPoints()
-			Status:SetInside(Bar, 2, 2)
-			Status:SetStatusBarTexture(C.Medias.Normal)
-			Status.SetStatusBarTexture = function() return end
-				
-			Text:ClearAllPoints()
-			Text:SetPoint("CENTER", Bar)
+	-- local originaTexture = MirrorTimerAtlas[timer]
 
-			Border:SetTexture(nil)
+	Bar:StripTextures()
+	Bar:CreateBackdrop()
+	Bar.Backdrop:SetBackdropColor(unpack(C.General.BackdropColor))
+	Bar.Backdrop:CreateShadow()
 
-			Bar.isSkinned = true
-		end
+	if (Bar.StatusBar) then
+		Bar.StatusBar:ClearAllPoints()
+		Bar.StatusBar:SetInside(Bar, 2, 2)
+		Bar.StatusBar:SetStatusBarTexture(C.Medias.Normal)
+		Bar.StatusBar.SetStatusBarTexture = function() return end
 	end
+	
+	if (Bar.Text) then
+		Bar.Text:ClearAllPoints()
+		Bar.Text:SetPoint("CENTER", Bar, "CENTER", 0, 0)
+	end
+
+	if (Bar.Border) then
+		Bar.Border:SetTexture(nil)
+	end
+
+	Bar.isSkinned = true
 end
 
 function MirrorTimers:Enable()
-	hooksecurefunc("MirrorTimer_Show", MirrorTimers.Update)
+	hooksecurefunc(MirrorTimerContainer, "SetupTimer", MirrorTimers.SetupTimer)
 end
 
 Miscellaneous.MirrorTimers = MirrorTimers
