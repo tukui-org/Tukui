9816f69c8286c33269bb012c6bc88679df3b51e2
pedrozc90 <pedrozc90@gmail.com>
2023-07-12 23:14:53 -0300

    keeping backwards compatible with classic/wrath

--- Tukui/Modules/Miscellaneous/MirrorTimers.lua
+++ Tukui/Modules/Miscellaneous/MirrorTimers.lua
@@ -3,41 +3,76 @@ local T, C, L = unpack((select(2, ...)))
 local Miscellaneous = T["Miscellaneous"]
 local MirrorTimers = CreateFrame("Frame")
 
-local MirrorTimerContainer = _G.MirrorTimerContainer
-local MirrorTimerAtlas = _G.MirrorTimerAtlas
+if (T.Retail) then
+	local MirrorTimerContainer = _G.MirrorTimerContainer
+	local MirrorTimerAtlas = _G.MirrorTimerAtlas
 
-function MirrorTimers:SetupTimer(timer, value, maxvalue, paused, label)
-	local Bar = MirrorTimerContainer:GetAvailableTimer(timer);
-	if (not Bar) then return end
+	function MirrorTimers:SetupTimer(timer, value, maxvalue, paused, label)
+		local Bar = self:GetAvailableTimer(timer);
+		if (not Bar) then return end
 
-	-- local originaTexture = MirrorTimerAtlas[timer]
+		-- local originaTexture = MirrorTimerAtlas[timer]
 
-	Bar:StripTextures()
-	Bar:CreateBackdrop()
-	Bar.Backdrop:SetBackdropColor(unpack(C.General.BackdropColor))
-	Bar.Backdrop:CreateShadow()
+		Bar:StripTextures()
+		Bar:CreateBackdrop()
+		Bar.Backdrop:SetBackdropColor(unpack(C.General.BackdropColor))
+		Bar.Backdrop:CreateShadow()
 
-	if (Bar.StatusBar) then
-		Bar.StatusBar:ClearAllPoints()
-		Bar.StatusBar:SetInside(Bar, 2, 2)
-		Bar.StatusBar:SetStatusBarTexture(C.Medias.Normal)
-		Bar.StatusBar.SetStatusBarTexture = function() return end
+		if (Bar.StatusBar) then
+			Bar.StatusBar:ClearAllPoints()
+			Bar.StatusBar:SetInside(Bar, 2, 2)
+			Bar.StatusBar:SetStatusBarTexture(C.Medias.Normal)
+			Bar.StatusBar.SetStatusBarTexture = function() return end
+		end
+		
+		if (Bar.Text) then
+			Bar.Text:ClearAllPoints()
+			Bar.Text:SetPoint("CENTER", Bar, "CENTER", 0, 0)
+		end
+
+		if (Bar.Border) then
+			Bar.Border:SetTexture(nil)
+		end
+
+		Bar.isSkinned = true
 	end
-	
-	if (Bar.Text) then
-		Bar.Text:ClearAllPoints()
-		Bar.Text:SetPoint("CENTER", Bar, "CENTER", 0, 0)
+
+	function MirrorTimers:Enable()
+		hooksecurefunc(MirrorTimerContainer, "SetupTimer", self.SetupTimer)
+	end
+else
+	function MirrorTimers:Update()
+		for i = 1, MIRRORTIMER_NUMTIMERS, 1 do
+			local Bar = _G["MirrorTimer"..i]
+			
+			if Bar and not Bar.isSkinned then
+				local Status = Bar.StatusBar or _G[Bar:GetName().."StatusBar"]
+				local Border = Bar.Border or _G[Bar:GetName().."Border"]
+				local Text = Bar.Text or _G[Bar:GetName().."Text"]
+
+				Bar:StripTextures()
+				Bar:CreateBackdrop()
+				Bar.Backdrop:SetBackdropColor(unpack(C.General.BackdropColor))
+				Bar.Backdrop:CreateShadow()
+
+				Status:ClearAllPoints()
+				Status:SetInside(Bar, 2, 2)
+				Status:SetStatusBarTexture(C.Medias.Normal)
+				Status.SetStatusBarTexture = function() return end
+					
+				Text:ClearAllPoints()
+				Text:SetPoint("CENTER", Bar)
+
+				Border:SetTexture(nil)
+
+				Bar.isSkinned = true
+			end
+		end
 	end
 
-	if (Bar.Border) then
-		Bar.Border:SetTexture(nil)
+	function MirrorTimers:Enable()
+		hooksecurefunc("MirrorTimer_Show", self.Update)
 	end
-
-	Bar.isSkinned = true
-end
-
-function MirrorTimers:Enable()
-	hooksecurefunc(MirrorTimerContainer, "SetupTimer", MirrorTimers.SetupTimer)
 end
 
 Miscellaneous.MirrorTimers = MirrorTimers
