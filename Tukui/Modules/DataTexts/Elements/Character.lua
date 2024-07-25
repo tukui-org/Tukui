fe7c5a545c146d342f85066eb68684683c364428
Tukz <tukz@tukui.org>
2024-07-24 20:07:13 -0400

    Update Character.lua

--- Tukui/Modules/DataTexts/Elements/Character.lua
+++ Tukui/Modules/DataTexts/Elements/Character.lua
@@ -4,31 +4,6 @@ local DataText = T["DataTexts"]
 local format = format
 local ClassColor = T.RGBToHex(unpack(T.Colors.class[T.MyClass]))
 
-local LeftTexts = {
-	"PlayerStatFrameLeft1",
-	"PlayerStatFrameLeft2",
-	"PlayerStatFrameLeft3",
-	"PlayerStatFrameLeft4",
-	"PlayerStatFrameLeft5",
-	"PlayerStatFrameLeft6",
-}
-local RightTexts = {
-	"PlayerStatFrameRight1",
-	"PlayerStatFrameRight2",
-	"PlayerStatFrameRight3",
-	"PlayerStatFrameRight4",
-	"PlayerStatFrameRight5",
-	"PlayerStatFrameRight6",
-}
-
-local CharacterStatFrame ={
-	"CharacterStatFrame1",
-	"CharacterStatFrame2",
-	"CharacterStatFrame3",
-	"CharacterStatFrame4",
-	"CharacterStatFrame5",
-}
-
 local Update = function(self)
 	local Total = 0
 	local Current, Max
@@ -57,143 +32,18 @@ local OnEnter = function(self)
 	GameTooltip:ClearLines()
 
 	if T.Retail then
-		local Objects = CharacterStatsPane.statsFramePool.activeObjects
-
-		-- Sort attributes
+		-- Set attributes (WIP)
 		GameTooltip:AddDoubleLine(ClassColor..T.MyName.."|r", T.MyRealm)
 		GameTooltip:AddLine(" ")
 		GameTooltip:AddLine("|CFFFFFFFF"..PET_BATTLE_STATS_LABEL.."|r")
 		GameTooltip:AddDoubleLine("|CF00FFF00"..LEVEL.."|r", UnitLevel("player"))
 		GameTooltip:AddDoubleLine("|CF00FFF00"..ITEM_UPGRADE_STAT_AVERAGE_ITEM_LEVEL.."|r", GetAverageItemLevel())
 		GameTooltip:AddLine(" ")
-		GameTooltip:AddLine("|CFFFFFFFF"..STAT_CATEGORY_ATTRIBUTES.."|r")
-
-		for Table in pairs(Objects) do
-			local Label = Table.Label:GetText()
-			local Value = Table.Value:GetText()
-			local Percent = string.find(Value, "%%")
-
-
-			if not Percent then
-				GameTooltip:AddDoubleLine("|CF00FFF00"..Label.."|r", "|CFFFFFFFF"..Value.."|r")
-			end
-		end
-
-		-- Sort enhancements
+		GameTooltip:AddDoubleLine("|CF00FFF00"..SPELL_STAT1_NAME.."|r", UnitStat("player", LE_UNIT_STAT_STRENGTH))
+		GameTooltip:AddDoubleLine("|CF00FFF00"..SPELL_STAT2_NAME.."|r", UnitStat("player", LE_UNIT_STAT_AGILITY))
+		GameTooltip:AddDoubleLine("|CF00FFF00"..SPELL_STAT3_NAME.."|r", UnitStat("player", LE_UNIT_STAT_INTELLECT))
+		GameTooltip:AddDoubleLine("|CF00FFF00"..SPELL_STAT4_NAME.."|r", UnitStat("player", LE_UNIT_STAT_STAMINA))
 		GameTooltip:AddLine(" ")
-		GameTooltip:AddLine("|CFFFFFFFF"..STAT_CATEGORY_ENHANCEMENTS.."|r")
-
-		for Table in pairs(Objects) do
-			local Label = Table.Label:GetText()
-			local Value = Table.Value:GetText()
-			local Percent = string.find(Value, "%%")
-
-			if Percent then
-				GameTooltip:AddDoubleLine("|CFFFFFF00"..Label.."|r", "|CFFFFFFFF"..Value.."|r")
-			end
-		end
-
-		GameTooltip:AddLine(" ")
-	end
-
-	if T.Cata then
-		local IsAlternativeTooltip = IsShiftKeyDown() or IsAltKeyDown()
-
-		-- GameTooltip:AddDoubleLine(ClassColor..T.MyName.."|r "..UnitLevel("player"), T.MyRealm)
-		-- GameTooltip:AddLine(" ")
-		-- for i = 1, 7 do
-		-- 	local hasStat = true
-		-- 	local j = 1
-
-		-- 	while hasStat do
-		-- 		local Name = _G["CharacterStatsPaneCategory" .. i .. "Stat" .. j .. "Label"]
-		-- 		local Value = _G["CharacterStatsPaneCategory" .. i .. "Stat" .. j .. "StatText"]
-		-- 		local StatName, StatValue
-
-		-- 		if Name and Name:GetText() then
-		-- 			StatName = "|cffff8000" .. Name:GetText() .. "|r"
-		-- 		end
-
-		-- 		if Value and Value:GetText() then
-		-- 			StatValue = "|cffffffff" .. Value:GetText() .. "|r"
-		-- 		end
-		-- 		if StatName and StatValue then
-		-- 			GameTooltip:AddDoubleLine("|CF00FFF00" .. StatName .. "|r", "|CFFFFFFFF" .. StatValue .. "|r")
-		-- 		else
-		-- 			hasStat = false
-		-- 		end
-		-- 		j = j + 1
-		-- 	end
-		-- 	GameTooltip:AddLine(" ")
-		-- end
-
-		-- Display left stats
-		-- for _, Frame in pairs(LeftTexts) do
-		-- 	local Name = _G[Frame.."Label"]
-		-- 	local Value = _G[Frame.."StatText"]
-		-- 	local Tooltip = _G[Frame].tooltip2
-		-- 	local StatName, StatValue
-
-		-- 	if Name:GetText() then
-		-- 		StatName = "|cffff8000"..Name:GetText().."|r"
-		-- 	end
-
-		-- 	if Value:GetText() then
-		-- 		StatValue = "|cffffffff"..Value:GetText().."|r"
-		-- 	end
-
-		-- 	if StatName and StatValue then
-		-- 		if IsAlternativeTooltip then
-		-- 			GameTooltip:AddLine("|CF00FFF00"..StatName.."|r |CFFFFFFFF"..StatValue.."|r")
-		-- 		else
-		-- 			GameTooltip:AddDoubleLine("|CF00FFF00"..StatName.."|r", "|CFFFFFFFF"..StatValue.."|r")
-		-- 		end
-
-		-- 		if Tooltip and IsAlternativeTooltip then
-		-- 			-- Remove double enter, for gaining tooltip space
-		-- 			Tooltip = string.gsub(Tooltip, "\n\n", " ")
-
-		-- 			GameTooltip:AddLine(Tooltip, .75, .75, .75)
-		-- 			GameTooltip:AddLine(" ")
-		-- 		end
-		-- 	end
-		-- end
-
-		-- -- Display right stats
-		-- for _, Frame in pairs(RightTexts) do
-		-- 	local Name = _G[Frame.."Label"]
-		-- 	local Value = _G[Frame.."StatText"]
-		-- 	local Tooltip = _G[Frame].tooltip2
-		-- 	local StatName, StatValue
-
-		-- 	if Name:GetText() then
-		-- 		StatName = "|cffff8000"..Name:GetText().."|r"
-		-- 	end
-
-		-- 	if Value:GetText() then
-		-- 		StatValue = "|cffffffff"..Value:GetText().."|r"
-		-- 	end
-
-		-- 	if StatName and StatValue then
-		-- 		if IsAlternativeTooltip then
-		-- 			GameTooltip:AddLine("|CF00FFF00"..StatName.."|r |CFFFFFFFF"..StatValue.."|r")
-		-- 		else
-		-- 			GameTooltip:AddDoubleLine("|CF00FFF00"..StatName.."|r", "|CFFFFFFFF"..StatValue.."|r")
-		-- 		end
-
-		-- 		if Tooltip and IsAlternativeTooltip then
-		-- 			-- Remove double enter, for gaining tooltip space
-		-- 			Tooltip = string.gsub(Tooltip, "\n\n", "\n")
-
-		-- 			GameTooltip:AddLine(Tooltip, .75, .75, .75)
-		-- 			GameTooltip:AddLine(" ")
-		-- 		end
-		-- 	end
-		-- end
-
-		-- if not IsShiftKeyDown() then
-		-- 	GameTooltip:AddLine(" ")
-		-- end
 	end
 
 	if T.Classic then
