e2237fc450fc8473bc2ade1bcc98ecec23c473bd
Tukz <tukz@tukui.org>
2024-07-25 23:53:44 -0400

    Update DropDown.lua

--- Tukui/Modules/Miscellaneous/DropDown.lua
+++ Tukui/Modules/Miscellaneous/DropDown.lua
@@ -13,73 +13,89 @@ DropDown.ChatMenus = {
 }
 
 function DropDown:Skin()
-	for i = 1, Levels do
-		local Backdrop
-
-		Backdrop = _G["DropDownList"..i.."MenuBackdrop"]
-		if Backdrop and not Backdrop.IsSkinned then
-			if Backdrop.NineSlice then
-				Backdrop.NineSlice:SetAlpha(0)
-			end
-
-			Backdrop:StripTextures()
-			Backdrop:CreateBackdrop("Default")
-			Backdrop:CreateShadow()
-			Backdrop.IsSkinned = true
+	if T.Retail then
+		local Dropdown = Menu.GetManager():GetOpenMenu()
+		
+		if Dropdown then
+			Dropdown:StripTextures()
+			Dropdown:SetTemplate()
 		end
+	else
+		for i = 1, Levels do
+			local Backdrop
 
-		Backdrop = _G["DropDownList"..i.."Backdrop"]
-		if Backdrop and not Backdrop.IsSkinned then
-			if Backdrop.NineSlice then
-				Backdrop.NineSlice:SetAlpha(0)
+			Backdrop = _G["DropDownList"..i.."MenuBackdrop"]
+			if Backdrop and not Backdrop.IsSkinned then
+				if Backdrop.NineSlice then
+					Backdrop.NineSlice:SetAlpha(0)
+				end
+
+				Backdrop:StripTextures()
+				Backdrop:CreateBackdrop("Default")
+				Backdrop:CreateShadow()
+				Backdrop.IsSkinned = true
 			end
 
-			Backdrop:StripTextures()
-			Backdrop:CreateBackdrop("Default")
-			Backdrop:CreateShadow()
-			Backdrop.IsSkinned = true
-		end
+			Backdrop = _G["DropDownList"..i.."Backdrop"]
+			if Backdrop and not Backdrop.IsSkinned then
+				if Backdrop.NineSlice then
+					Backdrop.NineSlice:SetAlpha(0)
+				end
 
-		Backdrop = _G["Lib_DropDownList"..i.."MenuBackdrop"]
-		if Backdrop and not Backdrop.IsSkinned then
-			if Backdrop.NineSlice then
-				Backdrop.NineSlice:SetAlpha(0)
+				Backdrop:StripTextures()
+				Backdrop:CreateBackdrop("Default")
+				Backdrop:CreateShadow()
+				Backdrop.IsSkinned = true
 			end
 
-			Backdrop:StripTextures()
-			Backdrop:CreateBackdrop("Default")
-			Backdrop:CreateShadow()
-			Backdrop.IsSkinned = true
-		end
+			Backdrop = _G["Lib_DropDownList"..i.."MenuBackdrop"]
+			if Backdrop and not Backdrop.IsSkinned then
+				if Backdrop.NineSlice then
+					Backdrop.NineSlice:SetAlpha(0)
+				end
 
-		Backdrop = _G["Lib_DropDownList"..i.."Backdrop"]
-		if Backdrop and not Backdrop.IsSkinned then
-			if Backdrop.NineSlice then
-				Backdrop.NineSlice:SetAlpha(0)
+				Backdrop:StripTextures()
+				Backdrop:CreateBackdrop("Default")
+				Backdrop:CreateShadow()
+				Backdrop.IsSkinned = true
 			end
 
-			Backdrop:StripTextures()
-			Backdrop:CreateBackdrop("Default")
-			Backdrop:CreateShadow()
-			Backdrop.IsSkinned = true
+			Backdrop = _G["Lib_DropDownList"..i.."Backdrop"]
+			if Backdrop and not Backdrop.IsSkinned then
+				if Backdrop.NineSlice then
+					Backdrop.NineSlice:SetAlpha(0)
+				end
+
+				Backdrop:StripTextures()
+				Backdrop:CreateBackdrop("Default")
+				Backdrop:CreateShadow()
+				Backdrop.IsSkinned = true
+			end
 		end
 	end
 end
 
 function DropDown:Enable()
-	local Menu
+	if T.Retail then
+		local Manager = Menu.GetManager()
+		
+		hooksecurefunc(Manager, "OpenMenu", self.Skin)
+		hooksecurefunc(Manager, "OpenContextMenu", self.Skin)
+	else
+		local Menu
+		
+		for i = 1, getn(self.ChatMenus) do
+			Menu = _G[self.ChatMenus[i]]
+			Menu:StripTextures()
+			Menu:CreateBackdrop()
+			Menu:CreateShadow()
+		end
 
-	for i = 1, getn(self.ChatMenus) do
-		Menu = _G[self.ChatMenus[i]]
-		Menu:StripTextures()
-		Menu:CreateBackdrop()
-		Menu:CreateShadow()
+		hooksecurefunc("UIDropDownMenu_CreateFrames", self.Skin)
+
+		-- use dropdown lib
+		self.Open = Lib_EasyMenu or EasyMenu
 	end
-
-	hooksecurefunc("UIDropDownMenu_CreateFrames", self.Skin)
-
-	-- use dropdown lib
-	self.Open = Lib_EasyMenu or EasyMenu
 end
 
 Miscellaneous.DropDown = DropDown
